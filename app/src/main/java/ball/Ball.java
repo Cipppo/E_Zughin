1de412925093d8e7653b9c909a269c78e2894657
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-24 19:32:18 +0100

    cleaning ToDos and commenting future features

--- app/src/main/java/ball/Ball.java
+++ app/src/main/java/ball/Ball.java
@@ -16,12 +16,12 @@ public class Ball extends Thread {
     private Time time = new Time(0.0, 0.0);
     private Boundary bounds;
     private Pos2D initialPosition;
-    private boolean stop = false;
+    //private boolean stop = false;
 
     // all temporary boundaries
     private static final double MAX_ANGLE = 80;
-    private static final double MIN_ANGLE = 30;
-    private static final double MAX_VELOCITY = 90;
+    private static final double MIN_ANGLE = 40;
+    private static final double MAX_VELOCITY = 150;
     private static final double MIN_VELOCITY = 50;
 
 
@@ -36,7 +36,16 @@ public class Ball extends Thread {
         this.velocity = this.trajectory.getXYVelocity();
 
     }
-
+    /**
+     * Need to implement thread support and consequently:
+     *      -a class for handling view/model interactions (update ball position -> draw ball position)
+     *          already created Visualiser class, need to change name of that and all Guis components;
+     *      -try to disassemble this class (thread delegation scares me) and build an entity and follow SRP
+     *          meaning that one class will handle movement and time, and another one is more like a structure that 
+     *          stores velocity values, (got to implement dimensions in the space), positions and maybe traj.
+     */
+    
+    /*
     @Override
     public void run() {
         try {
@@ -52,15 +61,17 @@ public class Ball extends Thread {
     public void terminate() {
         this.stop = true;
     }
+    */
 
     public synchronized void updatePos() {
         time.inc(0.3);
         this.actualPosition.x = this.initialPosition.x +  0.001 * this.velocity.getX() * this.time.x;
         this.actualPosition.y = this.initialPosition.y - 0.001 * (this.velocity.getY() * this.time.y - (0.5*gravity*Math.pow(this.time.y, 2)));
         this.applyConstraints();
-        System.out.println(this.actualPosition + " \t time: " + this.time + " \t "+ this.velocity);
+        //System.out.println(this.actualPosition + " \t time: " + this.time + " \t "+ this.velocity);
     }
 
+    // this should be cleand
     private void applyConstraints() {
         if(this.actualPosition.y > bounds.y1) {
             this.initialPosition.y = bounds.y1;
