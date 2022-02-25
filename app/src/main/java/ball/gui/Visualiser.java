2921bdb3600a43c97ac5485b120646a669e6a1c7
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-25 18:44:00 +0100

    Created Ball-Visual mediator

--- app/src/main/java/ball/gui/Visualiser.java
+++ app/src/main/java/ball/gui/Visualiser.java
@@ -1,10 +1,30 @@
 package ball.gui;
 
+import ball.ballAgent.BallAgent;
+
+/**
+ * This makes the BallAgent communicate with the Visual
+ */
 public class Visualiser extends Thread {
-    private Visual frame;
+    private final Visual frame;
+    private final BallAgent agent;
 
     public Visualiser() {
         this.frame = new Visual();
         this.frame.setVisible(true);
+        this.agent = new BallAgent();
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            this.agent.start();
+            while(true) {
+                this.frame.updatePosition(this.agent.getBallPosition());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
