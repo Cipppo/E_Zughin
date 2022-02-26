0de651a027abfb7ea1cc1d4b2fd5fde325436b1b
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-26 16:10:16 +0100

    switched boundaries into enumeration

--- app/src/main/java/ball/Boundary.java
+++ app/src/main/java/ball/Boundary.java
@@ -1,31 +1,19 @@
 package ball;
 
-public class Boundary {
-    public double x0;
-    public double y0;
-    public double x1;
-    public double y1;
-
-    public Boundary(double x0, double y0, double x1, double y1) {
-        this.x0 = x0;
-        this.y0 = y0;
-        this.x1 = x1;
-        this.y1 = y1;
-    }
-
-    public double getX0() {
-        return this.x0;
-    }
-
-    public double getY0() {
-        return this.y0;
-    }
-
-    public double getX1() {
-        return this.x1;
-    }
-
-    public double getY1() {
-        return this.y1;
-    }
+public enum Boundary {
+	X0(0),
+	Y0(0),
+	X1(1),
+	Y1(1);
+	
+	private final int n;
+	
+	private Boundary(final int n) {
+		this.n = n;
+	}
+	
+	public int getValue() {
+		return this.n;
+	}
+	
 }
