ee536c66dc88c2da4c5e5db6b91099e268a0c750
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-26 16:34:14 +0100

    first draft of this interface

---
+++ app/src/main/java/ball/physics/Entity.java
@@ -0,0 +1,28 @@
+package ball.physics;
+
+/**
+ * 
+ * need to define which thing is an entity. By now we've 
+ * defined that everything that occupies a 2 dimensional
+ * space could be described by this interface.
+ *
+ */
+public interface Entity {
+	
+	/**
+	 * 
+	 * @return the position occupied by this entity.
+	 */
+	Pos2D getPosition();
+	
+	/**
+	 * We got to define how to return the space occupied
+	 * by an entity. We could try to design everything as
+	 * a square and return the area to be calculated from 
+	 * the position occupied by it.
+	 * @return the area of the space occupied
+	 */
+	int getSize();
+	
+	
+}
