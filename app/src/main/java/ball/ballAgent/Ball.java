4323bd895e1a582c057e2a4da88a38aaf9b7db31
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-26 16:36:32 +0100

    adapded ball to enums and cleaning ToDos

--- app/src/main/java/ball/ballAgent/Ball.java
+++ app/src/main/java/ball/ballAgent/Ball.java
@@ -3,12 +3,25 @@ package ball.ballAgent;
 import ball.Boundary;
 import ball.physics.*;
 
+import java.awt.Dimension;
 import java.util.Random;
 
 enum Axis {
 	XAXIS,
 	YAXIS;
 }
+
+/**
+ * Need to implement thread support and consequently:
+ *      -TODO: try to disassemble this class (thread delegation scares me) and build an entity and follow SRP
+ *          meaning that one class will handle movement and time, and another one is more like a structure that 
+ *          stores velocity values, (got to implement dimensions in the space), positions and maybe traj.
+ *      -TODO: Need to implement starting Y value, and how to handle the first launch without compromising 
+ *      	initial velocity; (I think this could be optional);
+ *      -TODO: generate random x position and at a fixed height Y
+ *      -TODO: create a timer for suspension of balls during pre-game phase
+ *      -TODO: implement function that splits ball into other two with opposite Velocity2D.vx
+ */
 
 /**
  * This is the structure of the ball, described by:
@@ -20,17 +33,16 @@ public class Ball {
     private Pos2D actualPosition;
     private Velocity2D velocity;
 
-    private static final double GRAVITY = 9.81;
     private Time time = new Time(0.0, 0.0);
-    private Boundary bounds;
     private Pos2D initialPosition;
     //private boolean stop = false;
 
+    private static final double GRAVITY = 9.81;
     // all temporary boundaries
     private static final double MAX_ANGLE = 80;
-    private static final double MIN_ANGLE = 40;
+    private static final double MIN_ANGLE = 50;
     private static final double MAX_VELOCITY = 150;
-    private static final double MIN_VELOCITY = 50;
+    private static final double MIN_VELOCITY = 80;
 
 
     public Ball() {
@@ -38,19 +50,10 @@ public class Ball {
         var angle = rand.nextDouble() * (MAX_ANGLE - MIN_ANGLE) + MIN_ANGLE;
         var initialVelocity = rand.nextDouble() * (MAX_VELOCITY - MIN_VELOCITY) + MIN_VELOCITY;
         trajectory = new Trajectory(angle, initialVelocity);
-        this.bounds = new Boundary(0, 0, 1, 1);
-        this.actualPosition = new Pos2D(this.bounds.x0, this.bounds.y1);
-        this.initialPosition = new Pos2D(actualPosition.x, actualPosition.y);
+        this.actualPosition = new Pos2D(Boundary.X0.getValue(), Boundary.Y1.getValue(), Dimensions.FATHER);
+        this.initialPosition = new Pos2D(actualPosition.x, actualPosition.y, Dimensions.FATHER);
         this.velocity = this.trajectory.getXYVelocity();
     }
-    /**
-     * Need to implement thread support and consequently:
-     *      -try to disassemble this class (thread delegation scares me) and build an entity and follow SRP
-     *          meaning that one class will handle movement and time, and another one is more like a structure that 
-     *          stores velocity values, (got to implement dimensions in the space), positions and maybe traj.
-     *      -Need to implement starting Y value, and how to handle the first launch without compromising 
-     *      	initial velocity; (I think this could be optional);
-     */
 
     public synchronized void updatePos() {
         time.inc(0.2);
@@ -62,18 +65,17 @@ public class Ball {
 
     // this should be cleaned
     private void checkConstraints() {
-        if(this.actualPosition.y > bounds.y1) {
-            this.applyConstraints(bounds.y1, Axis.YAXIS);
-        } else if (this.actualPosition.y < bounds.y0) {
-            this.applyConstraints(bounds.y0, Axis.YAXIS);
-        } else if (this.actualPosition.x > bounds.x1 - 0.05) {
-            this.applyConstraints(bounds.x1 - 0.05, Axis.XAXIS);
-        } else if (this.actualPosition.x < bounds.x0) {
-            this.applyConstraints(bounds.x0, Axis.XAXIS);
+        if(this.actualPosition.y > Boundary.Y1.getValue()) {
+            this.applyConstraints(Boundary.Y1.getValue(), Axis.YAXIS);
+        } else if (this.actualPosition.y < Boundary.Y0.getValue()) {
+            this.applyConstraints(Boundary.Y0.getValue(), Axis.YAXIS);
+        } else if (this.actualPosition.x > Boundary.X1.getValue() - 0.05) {
+            this.applyConstraints(Boundary.X1.getValue() - 0.05, Axis.XAXIS);
+        } else if (this.actualPosition.x < Boundary.X0.getValue()) {
+            this.applyConstraints(Boundary.X0.getValue(), Axis.XAXIS);
         }
     }
 
-    // gotta find something better than that int
     private void applyConstraints(double bound, Axis axis) {
         if (axis == Axis.YAXIS) {
             this.initialPosition.y = bound;
