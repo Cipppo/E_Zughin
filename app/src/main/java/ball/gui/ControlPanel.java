3793d39482013451622eefe9c27d983abd2f0501
Stefano Furi <stefano.furi@studio.unibo.it>
2022-02-26 16:49:14 +0100

    control panel for controlling (by now) explosion of a ball

---
+++ app/src/main/java/ball/gui/ControlPanel.java
@@ -0,0 +1,31 @@
+package ball.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class ControlPanel extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JButton separateOne;
+	private Visualiser visual;
+	
+	public ControlPanel(Visualiser visual) {
+		this.visual = visual;
+		
+		this.setTitle("Control Panel");
+		this.setSize(250, 60);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.separateOne = new JButton("explode a ball");
+		
+		JPanel pane = new JPanel();
+		pane.add(this.separateOne);
+		this.add(pane);
+		
+		this.separateOne.addActionListener(t -> {
+			visual.removeBall();
+		});
+	}
+}
