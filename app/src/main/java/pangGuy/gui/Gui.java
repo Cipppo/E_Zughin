a4197fb1c7623a36d2ad56d4f86424c2e72cc6a8
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-01 10:43:11 +0100

    Implemented a simple gui

--- app/src/main/java/pangGuy/gui/Gui.java
+++ app/src/main/java/pangGuy/gui/Gui.java
@@ -1,28 +1,48 @@
 package pangGuy.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Gui extends JFrame{
+
+
+
+public class Gui {
 
     private static final long serialVersionUID = 1L;
     
     private static final int SIZEX = 800;
     private static final int SIZEY = 600;
+    private JButton jb = new JButton("OKOKKOKOKOKOK");
+    private JPanel panel = new JPanel();
+    private JFrame frame;
     
     public Gui() {
-        this.setTitle("Pang guy");
-        this.setSize(SIZEX, SIZEY);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+     
+        JFrame frame=new JFrame();  
+                          
+        // Creating Button          
+        JButton b = new JButton();
         
-        this.setResizable(false);
+        b.setBounds(50,50,90, 50);  
+             
+        //Adding button onto the frame
+        frame.add(b);  
+                  
+        // Setting Frame size. This is the window size
+        frame.setSize(300,200);  
         
-        
-        
-        this.setVisible(true);
+        frame.setLayout(null);  
+        frame.setVisible(true);  
+                  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
-    
-    
-
+        
 }
