19c6d8298667ee8fb1b535925f3e83270963e844
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-01 14:17:21 +0100

    Solved gui problems

--- app/src/main/java/pangGuy/gui/Gui.java
+++ app/src/main/java/pangGuy/gui/Gui.java
@@ -3,9 +3,8 @@ package pangGuy.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Polygon;
-import java.awt.RenderingHints;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,35 +13,62 @@ import javax.swing.JPanel;
 
 
 
-public class Gui {
+public class Gui extends JFrame{
 
     private static final long serialVersionUID = 1L;
     
     private static final int SIZEX = 800;
     private static final int SIZEY = 600;
     private JButton jb = new JButton("OKOKKOKOKOKOK");
-    private JPanel panel = new JPanel();
-    private JFrame frame;
+    private VisualPanel pan = new VisualPanel();
+    
     
     public Gui() {
-     
-        JFrame frame=new JFrame();  
-                          
-        // Creating Button          
-        JButton b = new JButton();
+        this.setSize(SIZEX, SIZEY);
+        this.setTitle("Pang Guy gui");
+        this.setLayout(new BorderLayout());
         
-        b.setBounds(50,50,90, 50);  
-             
-        //Adding button onto the frame
-        frame.add(b);  
-                  
-        // Setting Frame size. This is the window size
-        frame.setSize(300,200);  
+
+        this.getContentPane().add(this.jb, BorderLayout.SOUTH);
+        this.getContentPane().add(this.pan, BorderLayout.NORTH);
+
+        this.jb.addActionListener(e ->{
+
+            this.pan.up();
+            System.out.println("Done");
+
+        });
+
+
         
-        frame.setLayout(null);  
-        frame.setVisible(true);  
-                  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);  
+        
+        
+    }
+
+    public class VisualPanel extends JPanel{
+
+        public VisualPanel(){
+            this.setSize(SIZEX, SIZEY);
+            
+        }
+
+        public void paint(Graphics g){
+
+            
+            g.setColor(Color.green);
+
+
+            g.fillOval(100, 200, 40, 20);
+        }
+        
+        public void up(){
+            this.repaint();
+        }
+
+
     }
         
 }
