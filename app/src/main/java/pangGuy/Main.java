1103c3951e12d507240698490c18f7a943e96c5e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-04 15:07:12 +0100

    Fixed Indentation

--- app/src/main/java/pangGuy/Main.java
+++ app/src/main/java/pangGuy/Main.java
@@ -1,10 +1,13 @@
 package pangGuy;
 import pangGuy.gui.*;
 
+
 public class Main {
     
     public static void main(String[] args) {
       Gui gui = new Gui();
+
+      
       ControlPanel cp = new ControlPanel(gui);
        
    }
