93092d812e83a0926781f05c6d4ed3818c5e2ae5
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-04 20:07:50 +0100

    Created a simple Position class

---
+++ app/src/main/java/pangGuy/utilities/Pos2D.java
@@ -0,0 +1,15 @@
+package pangGuy.utilities;
+
+public class Pos2D{
+
+    public final int x;
+    public final int y;
+
+
+    public Pos2D(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
