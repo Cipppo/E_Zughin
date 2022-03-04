9d8a516cbe933fc0ac9d78366fd2352652da8ce3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-04 15:07:50 +0100

    Insert buttons

--- app/src/main/java/pangGuy/gui/ControlPanel.java
+++ app/src/main/java/pangGuy/gui/ControlPanel.java
@@ -1,31 +1,43 @@
 package pangGuy.gui;
 
 import javax.swing.JFrame;
+
+import pangGuy.utilities.Directions;
+
 import javax.swing.JButton;
+import java.awt.BorderLayout;
 
 public class ControlPanel extends JFrame{
 
-    private static final int SIZEX = 100;
-    private static final int SIZEY = 100;
+    private static final int SIZEX = 300;
+    private static final int SIZEY = 300;
 
     private JButton down = new JButton("Down");
     private JButton up = new JButton("Up");
     private JButton left = new JButton("Left");
     private JButton right = new JButton("right");
+    private final JButton center = new JButton("Controller");
     private Gui gui;
 
     public ControlPanel(Gui gui){
         this.gui = gui;
         this.setSize(SIZEX, SIZEY);
         this.setName("Control Panel");
+        this.setLayout(new BorderLayout());
         
         down.addActionListener(e -> {
-            this.gui.update();
+            this.gui.update(Directions.DOWN);
         });
 
-
-        this.getContentPane().add(this.down);
         
+
+
+        this.getContentPane().add(this.down, BorderLayout.SOUTH);
+        this.getContentPane().add(this.up, BorderLayout.NORTH);
+        this.getContentPane().add(this.left, BorderLayout.WEST);
+        this.getContentPane().add(this.right, BorderLayout.EAST);
+        this.getContentPane().add(this.center, BorderLayout.CENTER);
+
         this.setVisible(true);
     }
     
