673a025732884f2c3458da315758214dcb38877a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-05 15:34:20 +0100

    Fixed GUI warnings

--- app/src/main/java/pangGuy/gui/Actor.java
+++ app/src/main/java/pangGuy/gui/Actor.java
@@ -1,22 +1,33 @@
 package pangGuy.gui;
 
-import javax.swing.JPanel;
+
 
 import java.awt.Color;
 
 import javax.swing.JLabel;
+import javax.swing.KeyStroke;
 
 public class Actor extends JLabel{
     
     private final Shape s;
+    private ActionFactory f;
+
 
     public Actor(Shape s){
         this.s = s;
+        f = new ActionFactory();
 
         super.setBackground(Color.black);
         super.setBounds(this.s.getRectangle());
         super.setOpaque(true);
-    }
 
+        
+
+        super.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
+        super.getActionMap().put("upAction", f.getUpAction(s));
+        System.out.println(s.getPos().y);
+
+
+    }
 
 }
--- app/src/main/java/pangGuy/gui/Gui.java
+++ app/src/main/java/pangGuy/gui/Gui.java
@@ -1,14 +1,8 @@
 package pangGuy.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Polygon;
 import javax.swing.JFrame;
 import pangGuy.Guy;
-import pangGuy.utilities.Directions;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import pangGuy.utilities.Pos2D;
 
 
 
@@ -19,17 +13,26 @@ public class Gui extends JFrame{
     
     private static final int SIZEX = 800;
     private static final int SIZEY = 600;
-    private static final int DIM = 100;
-    
-    private final Guy guy = new Guy(100, 100);
 
+    private static final int STARTX = 100;
+    private static final int STARTY = 100;
+
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 100;
+
+    
+    private final Guy guy = new Guy(STARTX, STARTY);
+
+    private final Shape shape = new Shape(new Pos2D(STARTX, STARTY), WIDTH, HEIGHT);
+    private final Actor actor = new Actor(this.shape);
+    
 
     
     public Gui() {
         this.setSize(SIZEX, SIZEY);
         this.setTitle("Pang Guy gui");
-        this.setLayout(new BorderLayout());
-        
+        this.setLayout(null);
+        this.add(this.actor);
 
         
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -39,34 +42,6 @@ public class Gui extends JFrame{
     }
 
 
-    public void paint(Graphics g){
-        
-        //g.clearRect(this.x - 30, this.y + 30, 130, 130);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
-        g2.clearRect(0, 0, SIZEX, SIZEY);
-        g.setColor(Color.black);
-
-        synchronized (this){
-            Polygon p = new Polygon();
-            int x = this.guy.getPositions().x;
-            int y = this.guy.getPositions().y;
-
-            p.addPoint(x, y);
-            p.addPoint(x, y + DIM);
-            p.addPoint(x + DIM, y + DIM);
-            p.addPoint(x + DIM, y);
-            g.drawPolygon(p);
-
-        }
-    
-    }
-
-    public void update(Directions d){
-        //this.removeAll();
-        this.repaint();
-        this.guy.move(d);
-    }
+   
 }
