6ea59390843904066994391e1ad35d9bbb93e738
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-05 20:06:18 +0100

    Clean Actor

--- app/src/main/java/pangGuy/gui/Actor.java
+++ app/src/main/java/pangGuy/gui/Actor.java
@@ -5,29 +5,23 @@ package pangGuy.gui;
 import java.awt.Color;
 
 import javax.swing.JLabel;
-import javax.swing.KeyStroke;
 
 public class Actor extends JLabel{
     
     private final Shape s;
-    private ActionFactory f;
+
 
 
     public Actor(Shape s){
         this.s = s;
-        f = new ActionFactory();
+
 
         super.setBackground(Color.black);
         super.setBounds(this.s.getRectangle());
         super.setOpaque(true);
 
+
         
-
-        super.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
-        super.getActionMap().put("upAction", f.getUpAction(s));
-        System.out.println(s.getPos().y);
-
-
     }
 
 }
