618eab57cb90d217857030480120a26f7ef2e8ca
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-06 12:04:42 +0100

    Add BoundChecker and move method

--- app/src/main/java/pangGuy/gui/Actor.java
+++ app/src/main/java/pangGuy/gui/Actor.java
@@ -6,22 +6,37 @@ import java.awt.Color;
 
 import javax.swing.JLabel;
 
+import pangGuy.utilities.Pos2D;
+
 public class Actor extends JLabel{
     
-    private final Shape s;
+    private Shape s;
+    private final BoundChecker bChecker;
 
 
-
-    public Actor(Shape s){
+    public Actor(Shape s, BoundChecker bc){
         this.s = s;
-
+        this.bChecker = bc;
 
         super.setBackground(Color.black);
         super.setBounds(this.s.getRectangle());
         super.setOpaque(true);
-
+        
 
         
+    }
+
+    
+    public void changeLocation(Pos2D pos){
+        if(bChecker.isInside(pos, this.s.getDimensions().getX(), this.s.getDimensions().getY()));{
+            super.setLocation(pos.x, pos.y);
+            this.s = new Shape(pos, this.s.getDimensions().getX(), 
+            this.s.getDimensions().getY());
+        }
+    }
+
+    public Shape getShape(){
+        return this.s;
     }
 
 }
