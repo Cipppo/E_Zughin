d61b74fa317413eee92808b042ca2a43d93d0dd4
Stefano Furi <stefano.furi@studio.unibo.it>
2022-03-10 17:11:50 +0100

    First draft of an image loader (WORKS ONLY LOCALLY, if you want to run, modify the path of resources)

---
+++ app/src/main/java/ball/gui/ImageLoader.java
@@ -0,0 +1,41 @@
+package ball.gui;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import ball.physics.Dimensions;
+
+public class ImageLoader {
+    //TODO: transform this static path, into a dynamic following FontLoader 
+    private final String resourcesPath = "/Users/stefanofuri/OOP-prj/E_Zughin/app/src/main/resources/sprite/spheres/";
+
+    public ImageLoader() {
+    }
+
+    public BufferedImage getImageFromDimensions(Dimensions dimension) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(this.resourcesPath + this.getFileNameGivenDimension(dimension)));
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid dimension given");
+        }
+        return image;
+    }
+
+    private String getFileNameGivenDimension(Dimensions dim) {
+        switch (dim) {
+            case FATHER:
+                return "ballFather.png";
+            case SON:
+                return "ballSon.png";
+            case GRANDSON:
+                return "ballGrandSon.png";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}
