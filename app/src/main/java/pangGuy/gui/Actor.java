14eb7f5b0ba007382d08f6329e24ea4313140172
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-13 18:06:12 +0100

    Removed the usage of BoundChecker in order to increase encapsulation

--- app/src/main/java/pangGuy/gui/Actor.java
+++ app/src/main/java/pangGuy/gui/Actor.java
@@ -8,16 +8,18 @@ import javax.swing.JLabel;
 
 import pangGuy.utilities.Pos2D;
 
+
+
 public class Actor extends JLabel{
     
     private Shape s;
-    private final BoundChecker bChecker;
 
 
-    public Actor(Shape s, BoundChecker bc){
+
+
+    public Actor(Shape s){
         this.s = s;
-        this.bChecker = bc;
-
+        
         super.setBackground(Color.black);
         super.setBounds(this.s.getRectangle());
         super.setOpaque(true);
@@ -28,17 +30,18 @@ public class Actor extends JLabel{
 
     
     public void changeLocation(Pos2D pos){
-        if(bChecker.isInside(pos, this.s.getDimensions().getX(), this.s.getDimensions().getY())){
-            super.setLocation(pos.x, pos.y);
-            this.s = new Shape(pos, this.s.getDimensions().getX(), 
-            this.s.getDimensions().getY());
-        }else{
-            System.out.println("OUT");
-        }
+
+        super.setLocation(pos.x, pos.y);
+        this.s = new Shape(pos, this.s.getDimensions().getX(), 
+        this.s.getDimensions().getY());
+
+    
     }
 
     public Shape getShape(){
         return this.s;
     }
+
+
 
 }
