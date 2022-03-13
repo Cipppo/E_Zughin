766bdddd2ab2bedb6177d3c04399e60325c53725
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-13 18:10:28 +0100

    Comment methods in order to move the Actor in only 2 ways

--- app/src/main/java/pangGuy/actions/ActionApplier.java
+++ app/src/main/java/pangGuy/actions/ActionApplier.java
@@ -10,17 +10,21 @@ public class ActionApplier{
 
     public ActionApplier(Actor a, Moover m,  ActionFactory f){
 
+        /*
         a.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
         a.getActionMap().put("upAction", f.getUpAction(m));
 
         a.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "downAction");
         a.getActionMap().put("downAction", f.getDownAction(m));
 
+        */
+
         a.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "leftAction");
         a.getActionMap().put("leftAction", f.getLeftAction(m));
 
         a.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "rightAction");
         a.getActionMap().put("rightAction", f.getRightAction(m));
+        
     }
 
 
