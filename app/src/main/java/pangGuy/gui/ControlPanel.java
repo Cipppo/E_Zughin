59844b821bcab8cff5d6d678dede92ba9d2a4528
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-19 16:53:56 +0100

    Activate ControlPanel in order to help debug

--- app/src/main/java/pangGuy/gui/ControlPanel.java
+++ app/src/main/java/pangGuy/gui/ControlPanel.java
@@ -1,58 +1,33 @@
-/*
+
 
 package pangGuy.gui;
 
 import javax.swing.JFrame;
 
-import pangGuy.utilities.Directions;
+
 
 import javax.swing.JButton;
-import java.awt.BorderLayout;
+
 
 public class ControlPanel extends JFrame{
 
     private static final int SIZEX = 300;
     private static final int SIZEY = 300;
 
-    private JButton down = new JButton("Down");
-    private JButton up = new JButton("Up");
-    private JButton left = new JButton("Left");
-    private JButton right = new JButton("right");
-    private final JButton center = new JButton("Controller");
+    private JButton jb = new JButton("Raise");
     private Gui gui;
 
     public ControlPanel(Gui gui){
         this.gui = gui;
         this.setSize(SIZEX, SIZEY);
         this.setName("Control Panel");
-        this.setLayout(new BorderLayout());
         
-        down.addActionListener(e -> {
-            this.gui.update(Directions.DOWN);
+        this.jb.addActionListener(e -> {
+            this.gui.gun.raise();
         });
 
-        up.addActionListener(e -> {
-            this.gui.update(Directions.UP);
-        });
-
-        left.addActionListener(e -> {
-            this.gui.update(Directions.LEFT);
-        });
-
-        right.addActionListener(e -> {
-            this.gui.update(Directions.RIGHT);
-        });
-        
-
-
-        this.getContentPane().add(this.down, BorderLayout.SOUTH);
-        this.getContentPane().add(this.up, BorderLayout.NORTH);
-        this.getContentPane().add(this.left, BorderLayout.WEST);
-        this.getContentPane().add(this.right, BorderLayout.EAST);
-        this.getContentPane().add(this.center, BorderLayout.CENTER);
-
+        this.add(jb);
         this.setVisible(true);
     }
     
 }
-*/
