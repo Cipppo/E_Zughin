0cd5a2ea4396b6bde060669e0146df904e851529
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-19 21:20:20 +0100

    Add gunRaiser in constructor fields

--- app/src/main/java/pangGuy/actions/ActionApplier.java
+++ app/src/main/java/pangGuy/actions/ActionApplier.java
@@ -5,36 +5,24 @@ import javax.swing.KeyStroke;
 import pangGuy.gui.Actor;
 
 import pangGuy.gui.Moover;
+import pangGuy.gun.GunRaiser;
 
 public class ActionApplier{
 
-    public ActionApplier(Actor a, Moover m,  ActionFactory f){
-
-        /*
-        a.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
-        a.getActionMap().put("upAction", f.getUpAction(m));
-
-        a.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "downAction");
-        a.getActionMap().put("downAction", f.getDownAction(m));
-
-        */
+    public ActionApplier(Actor a, Moover m,  ActionFactory f, GunRaiser g){
 
         a.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "leftAction");
         a.getActionMap().put("leftAction", f.getLeftAction(m));
 
         a.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "rightAction");
         a.getActionMap().put("rightAction", f.getRightAction(m));
+
+        a.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "shootAction");
+        a.getActionMap().put("shootAction", f.getShootAction(g));
         
     }
 
 
 }
 
-       /*
-        super.setLocation(x, y); // -> Usare questo metodo per aggiornare!!!
-
-        super.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
-        super.getActionMap().put("upAction", f.getUpAction(s));
-        System.out.println(s.getPos().y);
-
-        */
+       
