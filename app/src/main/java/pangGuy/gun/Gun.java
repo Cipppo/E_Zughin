b156ad6969b5e2e86111f336eb11bd238e8684f4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-19 16:56:03 +0100

    Implemented 'Raise' method and fixed 'ChangeLocation' method

--- app/src/main/java/pangGuy/gun/Gun.java
+++ app/src/main/java/pangGuy/gun/Gun.java
@@ -12,11 +12,14 @@ import pangGuy.gui.Actor;
 public class Gun extends JPanel{
 
     private final Actor a;
+    private Shape s;
+    private boolean isMovable = true;
 
     public Gun(Actor actor){
         this.a = actor;
+        this.s = new Shape(this.a.getShape().getPos(), 10, 2);
 
-        super.setBounds(new Shape(this.a.getShape().getPos(), 10, 2).getRectangle());
+        super.setBounds(this.s.getRectangle());
         super.setBackground(Color.green);
         super.setForeground(Color.green);
         super.setOpaque(true);
@@ -24,7 +27,22 @@ public class Gun extends JPanel{
     }
 
     public void changeLocation(Pos2D pos){
-        super.setLocation(pos.x, pos.y);
+        if(this.isMovable){
+            super.setLocation(pos.x, pos.y);
+            this.s = new Shape(pos, s.getDimensions().getX(), s.getDimensions().getY());
+        }
+    }
+
+    public void raise(){
+        this.isMovable = false;
+        while(this.s.getPos().y > 0 ){
+            Pos2D newPos = new Pos2D(this.s.getPos().x , this.s.getPos().y -1);
+            this.s = new Shape(newPos, this.s.getDimensions().getX(), this.s.getDimensions().getY() + 1);
+            super.setBounds(this.s.getRectangle());
+            System.out.println(this.s.getPos().toString());
+        }
+        
+
     }
 
 
