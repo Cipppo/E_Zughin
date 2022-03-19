745df5c8feca479e2a9af92956eb72196bba6624
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-19 17:36:31 +0100

    Create 'Raiser' class which handle the bullet raise

---
+++ app/src/main/java/pangGuy/gun/Raiser.java
@@ -0,0 +1,33 @@
+package pangGuy.gun;
+
+import pangGuy.gui.BoundChecker;
+
+public class Raiser extends Thread{
+
+
+    private final Gun gun;
+    private final BoundChecker bc;
+
+
+    public Raiser(Gun gun, BoundChecker bc){
+        this.gun = gun;
+        this.bc = bc;
+    }
+
+
+    @Override
+    public void run(){
+        try{
+            this.gun.setUnMovable();
+            while(this.bc.isExtendible(this.gun.getPos())){
+                this.gun.raise();
+                Thread.sleep(20);
+            }
+            //this.gun.setMovable();
+        }catch (Exception e){
+            System.out.println("An error occured: " + e.getMessage());
+        }
+    }
+
+    
+}
