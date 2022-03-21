42ffe81af73793237cf0caa04a394b6afa7a19bc
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-03-21 19:40:12 +0100

    updating file bug fix

--- app/src/main/java/stage/utils/MainImagesLoader.java
+++ app/src/main/java/stage/utils/MainImagesLoader.java
@@ -13,7 +13,7 @@ public class MainImagesLoader {
 		SecondaryImagesLoader imagesLoader = new SecondaryImagesLoader();
 		
 		
-		try(var imageIn = MainImagesLoader.class.getResourceAsStream("/" + resourceName)) {
+		try(var imageIn = MainImagesLoader.class.getResourceAsStream(File.separator + resourceName)) {
 			image = ImageIO.read(new File(imageIn.toString()));
 			return image;
 		} catch (IOException e1) {
