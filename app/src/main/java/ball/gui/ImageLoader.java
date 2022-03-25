23ebc7a68566f5d7215dbe82c074f42dbefdf2a6
Stefano Furi <stefano.furi@studio.unibo.it>
2022-03-25 16:04:18 +0100

    Added loading using MainImagesLoader and implemented image caching

--- app/src/main/java/ball/gui/ImageLoader.java
+++ app/src/main/java/ball/gui/ImageLoader.java
@@ -1,32 +1,35 @@
 package ball.gui;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 import ball.physics.Dimensions;
+import ball.physics.Pos2D;
+import stage.utils.MainImagesLoader;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * this class loads the image of given ball (maybe try to disassemble for multiuse),
+ * and chaches the result of the loading;
+ */
 public class ImageLoader {
-    //TODO: transform this static path, into a dynamic following FontLoader 
-    private final String resourcesPath = "/Users/stefanofuri/OOP-prj/E_Zughin/app/src/main/resources/sprite/spheres/";
 
-    public ImageLoader() {
+    private Map<Pos2D, BufferedImage> entries = new HashMap<>();
+    private MainImagesLoader loader = new MainImagesLoader();
+
+    public void setBallImage(Pos2D ball) {
+        String spriteName = this.getFileNameGivenDimension(ball.getDimension());
+        BufferedImage image = loader.load(spriteName);
+        this.entries.put(ball, image);
+    }
+    
+    public BufferedImage getBallImage(Pos2D ball) {
+        return this.entries.get(ball);
     }
 
-    public BufferedImage getImageFromDimensions(Dimensions dimension) {
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(new File(this.resourcesPath + this.getFileNameGivenDimension(dimension)));
-        } catch (IOException e){
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid dimension given");
-        }
-        return image;
-    }
 
-    public String getFileNameGivenDimension(Dimensions dim) {
+    private String getFileNameGivenDimension(Dimensions dim) {
         switch (dim) {
             case FATHER:
                 return "ballFather.png";
@@ -38,4 +41,5 @@ public class ImageLoader {
                 throw new IllegalArgumentException();
         }
     }
+
 }
