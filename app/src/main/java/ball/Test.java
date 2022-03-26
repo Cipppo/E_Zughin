3e7daec9f1b975a7a3b8d10b50e65c699b65efcc
Stefano Furi <stefano.furi@studio.unibo.it>
2022-03-26 17:25:17 +0100

    updated tests

--- app/src/main/java/ball/Test.java
+++ app/src/main/java/ball/Test.java
@@ -1,14 +1,9 @@
 package ball;
-
-import ball.gui.ControlPanel;
 import ball.gui.Visualiser;
 
 public class Test {
     public static void main(String[] args) {
         Visualiser visual = new Visualiser(3);
         visual.start();
-        
-        ControlPanel control = new ControlPanel(visual);
-        control.setVisible(true);
     }
 }
