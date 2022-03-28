c2ba8992acc989d82ff088688cfb56c3fb712d56
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-28 12:08:58 +0200

    Deleted unused methods and add direction support

--- app/src/main/java/pangGuy/actions/ActionFactory.java
+++ app/src/main/java/pangGuy/actions/ActionFactory.java
@@ -6,29 +6,12 @@ import javax.swing.AbstractAction;
 
 import pangGuy.gui.Moover;
 import pangGuy.gun.GunRaiser;
+import pangGuy.utilities.Directions;
 import pangGuy.utilities.Pos2D;
 
 public class ActionFactory {
 
     private static final int SPEED = 5;
-
-
-    private class upAction extends AbstractAction{
-
-        private Moover m;
-
-        public upAction(Moover m){
-            this.m = m;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            m.move(new Pos2D(m.getCurrentPos().x, m.getCurrentPos().y - SPEED));
-            System.out.println("UP");
-            
-        }
-        
-    }
 
 
     private class rightAction extends AbstractAction{
@@ -41,7 +24,7 @@ public class ActionFactory {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            m.move(new Pos2D(m.getCurrentPos().x + SPEED, m.getCurrentPos().y));
+            m.move(new Pos2D(m.getCurrentPos().x + SPEED, m.getCurrentPos().y), Directions.RIGHT);
             System.out.println("RIGHT");
             
         }
@@ -58,28 +41,11 @@ public class ActionFactory {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            m.move(new Pos2D(m.getCurrentPos().x - SPEED, m.getCurrentPos().y));
+            m.move(new Pos2D(m.getCurrentPos().x - SPEED, m.getCurrentPos().y), Directions.LEFT);
             System.out.println("LEFT");
             
         }
         
-    }
-
-    private class downAction extends AbstractAction{
-        
-        private Moover m;
-
-        public downAction(Moover m){
-            this.m = m;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            m.move(new Pos2D(m.getCurrentPos().x,m.getCurrentPos().y + SPEED));
-            System.out.println("DOWN");
-            
-        }
-
     }
 
     private class shootAction extends AbstractAction{
@@ -97,9 +63,7 @@ public class ActionFactory {
 
     }
 
-    public upAction getUpAction(Moover a){
-        return new upAction(a);
-    }
+
 
     public rightAction getRightAction(Moover a){
         return new rightAction(a);
@@ -107,10 +71,6 @@ public class ActionFactory {
 
     public leftAction getLeftAction(Moover a){
         return new leftAction(a);
-    }
-
-    public downAction getDownAction(Moover a){
-        return new downAction(a);
     }
 
     public shootAction getShootAction(GunRaiser g){
