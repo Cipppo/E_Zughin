fa3c634e272e63243a33889d8bec7948ee6e0d4d
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-28 18:56:01 +0200

    Create the powerUp whichpermit me to shoot from 2 guns

---
+++ app/src/main/java/powerUp/DoubleGun.java
@@ -0,0 +1,46 @@
+package powerUp;
+
+import pangGuy.gun.GunRaiser;
+
+public class DoubleGun extends Thread implements PowerUp{
+
+    private final GunRaiser gunRaiser;
+    private int timeElapsed;
+    private int duration;
+
+    public DoubleGun(GunRaiser gunRaiser){
+        this.gunRaiser = gunRaiser;
+        this.timeElapsed = 0;
+        this.duration = 3;
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Double Gun Enabled");
+        this.gunRaiser.activateSecondGun();
+        while(!this.isActive()){
+            try{
+                System.out.print(".");
+                Thread.sleep(1000);
+            }catch(Exception e){
+                System.out.println("Thread.sleep() Exception: " + e.getMessage());
+            }
+            this.timeElapsed++;
+        }
+            System.out.println("Finished");
+            this.gunRaiser.deactivateSecondGun();
+
+    }
+
+    @Override
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean isActive() {
+        return this.timeElapsed == this.duration;
+    }
+
+    
+}
