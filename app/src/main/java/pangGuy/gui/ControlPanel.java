c14df2743d0844c128e43562c1e05ad86baef7e2
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-29 23:06:58 +0200

    Updated ControlPanel

--- app/src/main/java/pangGuy/gui/ControlPanel.java
+++ app/src/main/java/pangGuy/gui/ControlPanel.java
@@ -7,9 +7,12 @@ import javax.swing.JFrame;
 import pangGuy.gun.GunRaiser;
 import powerUp.DoubleGun;
 
+import java.awt.Color;
+
 import javax.swing.GrayFilter;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
 
 @SuppressWarnings("unused")
 public class ControlPanel extends JFrame{
@@ -21,23 +24,55 @@ public class ControlPanel extends JFrame{
     private Gui gui;
     private GunRaiser gRaiser;
     private JLabel label = new JLabel("Status");
+
     public ControlPanel(Gui gui){
         this.gui = gui;
         this.gRaiser = gui.gunRaiser;
+
         
+
         this.setSize(SIZEX, SIZEY);
         this.setName("Control Panel");
         
         this.jb.addActionListener(e -> {
             new DoubleGun(this.gRaiser).start();
+            new Banner(this.jb).start();
         });
 
         this.add(jb);
+        
+        
+
         this.setVisible(true);
     }
     
     private class Banner extends Thread{
         
+        private JButton jb;
+        private int timeElapsed;
+        private int totalTime;
+
+        public Banner(JButton jb){
+            this.jb = jb;
+            this.timeElapsed = 0;
+            this.totalTime = 15;
+        }
+
+
+        public void run(){
+            
+            try{
+                this.jb.setBackground(Color.green);
+                Thread.sleep(15000);
+                this.jb.setBackground(Color.gray);
+            }catch(Exception e){
+                System.out.println("\nThread.sleep() Exception: " + e.getMessage());
+            }
+            
+        }
+
+      
+
     }
 
 
