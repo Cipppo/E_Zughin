ad348eaa827ec5f081d8f852a52e74b50e4d2ce4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-30 20:53:54 +0200

    Create Bullet interface

--- app/src/main/java/pangGuy/modularGun/Bullet.java
+++ app/src/main/java/pangGuy/modularGun/Bullet.java
@@ -1,5 +1,22 @@
 package pangGuy.modularGun;
 
+import pangGuy.utilities.Pos2D;
+
 public interface Bullet {
     
+    Pos2D getPos();
+
+    void setPos();
+
+    void setMovable();
+
+    void setUnMovable();
+
+    boolean isMovable();
+
+    void restore();
+
+    void shoot();
+
+
 }
