7a964957dd9451c44fb5074b9f35aeeb36e2685c
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-30 21:23:26 +0200

    Start to implement the Arpion class

--- app/src/main/java/pangGuy/modularGun/Arpion.java
+++ app/src/main/java/pangGuy/modularGun/Arpion.java
@@ -2,50 +2,78 @@ package pangGuy.modularGun;
 
 import javax.swing.JPanel;
 
+import pangGuy.utilities.Directions;
 import pangGuy.utilities.Pos2D;
+import pangGuy.utilities.PosConverter;
+import pangGuy.utilities.Pair;
+import pangGuy.gui.Shape;
+import pangGuy.gui.Actor;
+
+import java.awt.Color;
 
 public class Arpion extends JPanel implements Bullet {
 
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 2;
+    private static final int SPEED = 4;
+
+    private final Actor actor;
+    private Shape s;
+    private boolean isMovable;
+    private Directions direction;
+
+    public Arpion(Actor actor, Color color){
+        super();
+        this.actor = actor;
+        this.s = new Shape(this.actor.getShape().getPos(), WIDTH, HEIGHT);
+        this.direction = Directions.LEFT;
+        this.isMovable = true;
+
+        super.setBounds(this.s.getRectangle());
+        super.setBackground(color);
+        super.setOpaque(true);
+    }
+
+    private void shapeUpdater(Pos2D pos){
+        this.s = new Shape(pos, s.getDimensions().getX(), s.getDimensions().getY());
+    }
+
     @Override
     public Pos2D getPos() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.s.getPos();
     }
 
     @Override
-    public void setPos() {
-        // TODO Auto-generated method stub
-        
+    public void setPos(Pos2D pos) {
+        if(this.isMovable){
+            super.setLocation(pos.x, pos.y);
+            this.shapeUpdater(pos);
+        }
     }
 
-    @Override
-    public void setMovable() {
-        // TODO Auto-generated method stub
-        
+    private void setUnMovable(){
+        this.isMovable = false;
     }
 
-    @Override
-    public void setUnMovable() {
-        // TODO Auto-generated method stub
-        
+    private void setMovable(){
+        this.isMovable = true;
     }
 
     @Override
     public boolean isMovable() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.isMovable;
     }
 
     @Override
     public void restore() {
-        // TODO Auto-generated method stub
-        
+        PosConverter conv = new PosConverter(new Pair<Integer>(WIDTH, HEIGHT), this.actor);
+        this.shapeUpdater(conv.getPos(this.direction));
+        super.setBounds(this.s.getRectangle());
     }
 
     @Override
     public void shoot() {
-        // TODO Auto-generated method stub
-        
+
     }
     
 }
