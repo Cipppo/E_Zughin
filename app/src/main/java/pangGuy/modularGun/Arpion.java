7f9b706228136aac7947ff108c96aec560d4702c
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-30 20:58:50 +0200

    Create Arpion Class

---
+++ app/src/main/java/pangGuy/modularGun/Arpion.java
@@ -0,0 +1,51 @@
+package pangGuy.modularGun;
+
+import javax.swing.JPanel;
+
+import pangGuy.utilities.Pos2D;
+
+public class Arpion extends JPanel implements Bullet {
+
+    @Override
+    public Pos2D getPos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setPos() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setMovable() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setUnMovable() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isMovable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void restore() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void shoot() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
