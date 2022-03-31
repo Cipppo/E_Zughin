d983910159ba60f2ed6f8d67fea74c610114cf3a
Stefano Furi <stefano.furi@studio.unibo.it>
2022-03-31 21:33:56 +0200

    now Position stores also radius

--- app/src/main/java/ball/physics/Pos2D.java
+++ app/src/main/java/ball/physics/Pos2D.java
@@ -13,12 +13,17 @@ public class Pos2D {
     public double x;
     public double y;
     private final Dimensions dimension;
+    private int radius;
 
-    
-    public Pos2D(final double x, final double y, Dimensions dimension) {
+    public Pos2D(final double x, final double y, Dimensions dimension, int radius) {
         this.x = x;
         this.y = y;
         this.dimension = dimension;
+        this.radius = radius;
+    }
+
+    public Pos2D(final double x, final double y, Dimensions dimension) {
+        this(x, y, dimension, 0);
     }
 
     public Dimensions getDimension() {
@@ -31,5 +36,33 @@ public class Pos2D {
     @Override
     public String toString() {
         return "Pos(" + this.x + ", " + this.y + ")";
+    }
+
+    /**
+     * Dimension Based on PNGs dimensions
+     * @return
+     *      the size of the ball
+     */
+    private int getDiamterFromDimension() {
+        switch (this.dimension) {
+            case FATHER:
+                this.radius = 50;
+                return 50;
+            case SON:
+                this.radius = 41;
+                return 41;
+            case GRANDSON:
+                this.radius = 25;
+                return 25;
+            default:
+                return 0;
+        }
+    }
+
+    public int getRadius() {
+        if (this.radius == 0) {
+            return this.getDiamterFromDimension();
+        }
+        return this.radius;
     }
 }
