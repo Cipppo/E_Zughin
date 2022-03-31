b29e298016dc9a4bb49a237d34ca4beacc8ce76f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-03-31 10:47:08 +0200

    Implementing GunSet btw gun bullet is missing

---
+++ app/src/main/java/pangGuy/modularGun/GunSet.java
@@ -0,0 +1,63 @@
+package pangGuy.modularGun;
+
+import java.awt.Color;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Optional;
+
+import pangGuy.gui.Actor;
+
+public class GunSet {
+
+    private final List<Bullet> arpions;
+    //private final Bullet gun; TODO after gun implementation
+    private final Actor actor;
+    private GunTypes currentGun;
+
+    public GunSet(Actor actor){
+        this.arpions = new ArrayList<Bullet>(
+            Set.of(new Arpion(actor, Color.green), new Arpion(actor, Color.orange))
+        );
+
+        this.actor = actor;
+        this.currentGun = GunTypes.ARPION;
+    }
+
+    private Optional<Bullet> getSingleArpion(){
+        if(this.currentGun == GunTypes.ARPION){
+            if(this.arpions.get(0).isMovable()){
+                return Optional.of(this.arpions.get(0));
+            }else{
+                return Optional.empty();
+            }
+        }else if(this.currentGun == GunTypes.DOUBLE_ARPION){
+            for(Bullet i : this.arpions){
+                if(i.isMovable()){
+                    return Optional.of(i);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    
+
+
+    public Set<Bullet> getShootingGun(){
+        Set<Bullet> shootingGuns = new HashSet<>();
+        if(this.currentGun == GunTypes.ARPION || this.currentGun == GunTypes.DOUBLE_ARPION){
+            if(!this.getSingleArpion().isEmpty()){
+                shootingGuns.add(getSingleArpion().get());
+            }
+        }
+        return null;        //GONNA DELETE IT ASAP, I JUST NEED IT AS A PATCH FOR THE "GUN" BULLET MISSING
+    }
+
+
+
+    
+}
