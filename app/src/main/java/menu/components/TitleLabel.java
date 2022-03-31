bd71fe999fb574689881276fa4cef345aa515ca7
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-03-31 21:37:35 +0200

    cleaning and updating classes in package menu.components

--- app/src/main/java/menu/components/TitleLabel.java
+++ app/src/main/java/menu/components/TitleLabel.java
@@ -1,14 +1,8 @@
 package menu.components;
 
-import java.awt.Font;
+import menu.utils.PrimaryLabel;
 
-import javax.swing.JLabel;
-
-import java.awt.Color;
-
-import stage.utils.MainFontLoader;
-
-public class TitleLabel extends JLabel{
+public class TitleLabel extends PrimaryLabel{
     
     
 	
@@ -16,12 +10,9 @@ public class TitleLabel extends JLabel{
 	 * 
 	 */
 	private static final long serialVersionUID = -9143946569687691268L;
-	MainFontLoader fontLoader = new MainFontLoader(11);
-	Font gameFont = fontLoader.load();
-    
+	
     public TitleLabel() {
         this.setText("PANG");
-		this.setForeground(Color.magenta);
-		this.setFont(gameFont);
+		this.setFont(this.getGameFont());
     }
 }
