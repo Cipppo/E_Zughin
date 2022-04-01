4343e0ef51f058a433544a9a5c1756231dccd1e2
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-01 18:37:07 +0200

    Add direction field to Actor with Setter and Getter

--- app/src/main/java/pangGuy/gui/Actor.java
+++ app/src/main/java/pangGuy/gui/Actor.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 
 import javax.swing.JLabel;
 
+import pangGuy.utilities.Directions;
 import pangGuy.utilities.Pos2D;
 
 
@@ -14,12 +15,14 @@ public class Actor extends JLabel{
     
     private Shape s;
 
+    private Directions direction;
 
 
 
     public Actor(Shape s){
         this.s = s;
-        
+        this.direction = Directions.LEFT;
+
         super.setBackground(Color.black);
         super.setBounds(this.s.getRectangle());
         super.setOpaque(true);
@@ -40,6 +43,13 @@ public class Actor extends JLabel{
         return this.s;
     }
 
+    public Directions getDir(){
+        return this.direction;
+    }
+
+    public void setDir(Directions dir){
+        this.direction = dir;
+    }
 
 
 }
