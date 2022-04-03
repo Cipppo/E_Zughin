64329609041f5120ba9afba379b127228957f65f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-03 17:17:10 +0200

    Updated ControlPanel adding the new Powerup

--- app/src/main/java/pangGuy/gui/ControlPanel.java
+++ app/src/main/java/pangGuy/gui/ControlPanel.java
@@ -6,12 +6,14 @@ import javax.swing.JFrame;
 
 import pangGuy.gun.GunRaiser;
 import powerUp.DoubleGun;
+import powerUp.StickyArpion;
 
 import java.awt.Color;
 
 import javax.swing.GrayFilter;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import pangGuy.modularGun.GunSet;
 @SuppressWarnings("unused")
@@ -21,9 +23,12 @@ public class ControlPanel extends JFrame{
     private static final int SIZEY = 300;
 
     private JButton jb = new JButton("Double Arpion: not Active");
+    private JButton jb2 = new JButton("Sticky Arpion: not Active");
     private Gui gui;
     private GunSet gSet;
     private JLabel label = new JLabel("Status");
+
+    private JPanel panel = new JPanel();
 
     public ControlPanel(Gui gui){
         this.gui = gui;
@@ -34,13 +39,21 @@ public class ControlPanel extends JFrame{
         this.setSize(SIZEX, SIZEY);
         this.setName("Control Panel");
         this.jb.setBackground(Color.red);
+        this.jb2.setBackground(Color.red);
         
         this.jb.addActionListener(e -> {
             new DoubleGun(this.gSet).start();
             new Banner(this.jb).start();
         });
+        
+        this.jb2.addActionListener(e -> {
+            new StickyArpion(this.gSet).start();
+            new Banner(this.jb2).start();
+        });
 
-        this.add(jb);
+        this.panel.add(jb);
+        this.panel.add(jb2);
+        this.getContentPane().add(this.panel);
         
         
 
