d970274a826224e86afb47e4f28bcc576f6403ec
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-04-03 10:24:30 +0200

    update gui primary class to test stuff

--- app/src/main/java/menu/gui/Gui.java
+++ app/src/main/java/menu/gui/Gui.java
@@ -1,14 +1,18 @@
 package menu.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 
 import menu.components.MainPanel;
+import menu.components.TestingKeyListener;
 import stage.utils.CenterOnDefaultScreen;
 
-public class Gui extends JFrame{
+public class Gui extends JFrame implements KeyListener{
 	
 	
 	
@@ -18,6 +22,10 @@ public class Gui extends JFrame{
 	private static final long serialVersionUID = 5959162590670099057L;
 	private final int width = CenterOnDefaultScreen.center().width;
 	private final int heigth = CenterOnDefaultScreen.center().height;
+	private MainPanel mainPanel;
+	
+	
+	private TestingKeyListener test;
 
 	public Gui() throws FileNotFoundException {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -26,9 +34,31 @@ public class Gui extends JFrame{
 		this.setLayout(new BorderLayout());
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
-		this.add(new MainPanel(), BorderLayout.CENTER);
 		
+		//this.mainPanel = new MainPanel(this);
+		test = new TestingKeyListener(this);
+		this.add(test, BorderLayout.CENTER);
+		//this.addKeyListener(this);
 		
 		this.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("you released key: " + e.getKeyChar());
+		
 	}
 }
