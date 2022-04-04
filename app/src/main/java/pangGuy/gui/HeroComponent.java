d72b040ddd873e44469faf9157fe281abb854db6
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-05 01:22:47 +0200

    Switch Extension from JComponent to JPanel

--- app/src/main/java/pangGuy/gui/HeroComponent.java
+++ app/src/main/java/pangGuy/gui/HeroComponent.java
@@ -1,7 +1,43 @@
 package pangGuy.gui;
 
-import javax.swing.JComponent;
+import java.awt.Color;
 
-public class HeroComponent extends JComponent{
-    
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import pangGuy.utilities.Directions;
+import pangGuy.utilities.Pos2D;
+
+public class HeroComponent extends JPanel{
+
+    private static final int WIDTH = 40;
+    private static final int HEIGHT = 40;
+
+    private Shape s;
+
+
+
+    public HeroComponent(Pos2D startPos){
+        this.s = new Shape(startPos, WIDTH, HEIGHT);
+
+        super.setBackground(Color.black);
+        super.setBounds(this.s.getRectangle());
+        super.setOpaque(true);
+
+    }
+
+
+    public void changeLocation(Pos2D pos){
+        super.setLocation(pos.x, pos.y);
+        this.s = new Shape(pos, WIDTH, HEIGHT);
+
+    }
+
+    public Shape getShape(){
+        return this.s;
+    }
+
+
+
+
 }
