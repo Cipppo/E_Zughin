d8808fcc6300bd5d0da4f408e7a7288327222c77
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-05 01:38:31 +0200

    Swap Actor with Shape

--- app/src/main/java/pangGuy/utilities/PosConverter.java
+++ app/src/main/java/pangGuy/utilities/PosConverter.java
@@ -1,27 +1,28 @@
 package pangGuy.utilities;
 
-import pangGuy.gui.Actor;
+
+import pangGuy.gui.Shape;
 
 
 public class PosConverter {
     
     private Pair<Integer, Integer> dimensions;
-    private Actor actor;
+    private Shape shape;
 
-    public PosConverter(Pair<Integer, Integer> dimensions, Actor actor){
+    public PosConverter(Pair<Integer, Integer> dimensions, Shape shape){
         this.dimensions = dimensions;
-        this.actor = actor;
+        this.shape = shape;
     }
 
 
     private Pos2D getLeftPos(){
-        return this.actor.getShape().getLeftFoot();
+        return this.shape.getLeftFoot();
     }
 
 
     private Pos2D getRightPos(){
-        int x = (this.actor.getShape().getLeftFoot().x + (int) this.actor.getShape().getDimensions().getX()) - this.dimensions.getX();
-        int y = this.actor.getShape().getLeftFoot().y;
+        int x = (this.shape.getLeftFoot().x + (int) this.shape.getDimensions().getX()) - this.dimensions.getX();
+        int y = this.shape.getLeftFoot().y;
         return new Pos2D(x, y);
     }
 
