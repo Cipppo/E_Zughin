9dc9f3933bf28a9f8f4a2af9e0579313a715da55
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-04-05 11:21:22 +0200

    Add NickNameinput

---
+++ app/src/main/java/menu/utils/NicknameInput.java
@@ -0,0 +1,87 @@
+package menu.utils;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class NicknameInput extends JPanel{
+
+	private static final long serialVersionUID = -3550165270008975447L;
+	private String nickname;
+	private int check;
+	private int seconds = 0;
+	private Timer timer;
+	private SecondaryLabel nick;
+	
+	public NicknameInput() {
+		nickname = "";
+		check = -1;
+		nick = new SecondaryLabel();
+		nick.setSSize(4);
+		nick.setFont(nick.getGameFont());
+		nick.setText(nickname);
+		this.setLayout(new BorderLayout());
+		this.add(nick, BorderLayout.CENTER);
+		this.setBackground(Color.black);
+		simpleTimer();
+		timer.start();
+		
+	}
+	
+	public void enterInput() {
+		if (check == -1) {
+			nickname += "  ";
+			nick.setText(nickname);
+		}
+	}
+		
+	public void addChar(char c) {
+		String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		String car = "" + c;
+		car = car.toUpperCase();
+		if (alpha.contains(car)) {
+			//nickname = nickname.substring(0, nickname.length()-1) + car + "-";
+			nickname += car;
+			nick.setText(nickname);
+		}
+	}
+	
+	public void deleteChar() {
+		if (nickname.length() > 2) {
+			//nickname = nickname.substring(0, nickname.length()-2) + "-";
+			nickname = nickname.substring(0, nickname.length()-1);
+			nick.setText(nickname);
+		}
+	}
+	
+	public void simpleTimer() {
+		timer = new Timer(500, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seconds++;
+                if (seconds % 2 == 0) {
+                	nick.setForeground(Color.GREEN);
+    			}else {
+    				nick.setForeground(Color.GREEN);
+    			}
+                
+                
+                if(seconds >= 1000) {
+                	//seconds = 0;
+                	timer.stop();
+                }
+            }
+
+        });
+	}
+}
+	
+	
+	
+
+
