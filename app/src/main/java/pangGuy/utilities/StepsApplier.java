ccbea44af23dde21c7269db40b65def3b39c8b6e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-05 02:54:53 +0200

    Add constructor

--- app/src/main/java/pangGuy/utilities/StepsApplier.java
+++ app/src/main/java/pangGuy/utilities/StepsApplier.java
@@ -4,12 +4,15 @@ public class StepsApplier {
 
     private static final int HERO_STEP = 5;
 
+    private Pos2D startPos;
 
-    public StepsApplier(){}
+    public StepsApplier(Pos2D startpos){
+        this.startPos = startpos;
+    }
 
     public Pos2D convertHeroPosition(Pos2D pos){
-        int x = Math.abs(pos.x * HERO_STEP);
-        return new Pos2D(x, pos.y);
+        int x = (pos.x * HERO_STEP) + this.startPos.x;
+        return new Pos2D(x, this.startPos.y);
     }
 
 
