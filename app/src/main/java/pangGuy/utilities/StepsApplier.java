9447744686790ad1c209795cf8a82208137abdf8
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-06 01:22:34 +0200

    Changed Bullet_step VALUE

--- app/src/main/java/pangGuy/utilities/StepsApplier.java
+++ app/src/main/java/pangGuy/utilities/StepsApplier.java
@@ -3,7 +3,7 @@ package pangGuy.utilities;
 public class StepsApplier {
 
     private static final int HERO_STEP = 5;
-    private static final int BULLET_STEP = 4;
+    private static final int BULLET_STEP = 5;
 
     private Pos2D startPos;
 
