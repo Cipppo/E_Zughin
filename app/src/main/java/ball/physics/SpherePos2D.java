ff66f14b256cc6fe6f7c5fc6770debbfd81ef949
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-07 21:34:35 +0200

    Implemented a Spherical Position

--- app/src/main/java/ball/physics/SpherePos2D.java
+++ app/src/main/java/ball/physics/SpherePos2D.java
@@ -1,42 +1,23 @@
 package ball.physics;
 
 /**
- * Got to implement space occupation by this entity 
- *  (maybe creating an interface "Entity" for the 
- *  main character, bird and this ball);
- *  TODO: modify to private fields please
- *  TODO: add a copy-of method for avoiding verbose creation in factory
- *  TODO: add a mehod for having area occupied by this entyty (could be useful for
- *  view (checking borders) and for checking collisions beetween balls and charachter);
+ * Modeling a sphere that occupies a place in 2 dimensional space
+ * and a size in space described by it's diameter
  */
-public class Pos2D {
-    public double x;
-    public double y;
-    private final Dimensions dimension;
-    private int radius;
+public class SpherePos2D extends Pos2D {
+    private int diameter;
 
-    public Pos2D(final double x, final double y, Dimensions dimension, int radius) {
-        this.x = x;
-        this.y = y;
-        this.dimension = dimension;
-        this.radius = (int)(radius * this.dimension.getValue());
+    public SpherePos2D(final double x, final double y, final Dimensions dimension, final int diameter) {
+        super(x, y, dimension);
+        this.diameter = (int)(diameter * super.getDimension().getValue());
     }
 
-    public Pos2D(final double x, final double y, Dimensions dimension) {
-        this(x, y, dimension, 0);
-    }
-
-    public Dimensions getDimension() {
-    	return this.dimension;
-    }
-
-    @Override
-    public String toString() {
-        return "Pos(" + this.x + ", " + this.y + ")";
+    public SpherePos2D(final double x, final double y, final Dimensions dimension) {
+        super(x, y, dimension);
     }
 
     public int getDiameter() {
-        return this.radius;
+        return this.diameter;
     }
 
 }
