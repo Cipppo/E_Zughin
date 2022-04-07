8510f0d295dcd3902231460125dafd8add359885
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-08 00:07:12 +0200

    Implemented simple visual

---
+++ app/src/main/java/mergeballs/gui/VisualTest.java
@@ -0,0 +1,144 @@
+package mergeballs.gui;
+
+import pangGuy.gui.ArpionComponent;
+import pangGuy.gui.HeroComponent;
+import pangGuy.modularGun.Status;
+import pangGuy.utilities.Directions;
+import pangGuy.utilities.Pair;
+import pangGuy.utilities.Pos2D;
+
+import java.util.List;
+
+import ball.gui.ImageLoader;
+import ball.gui.Visual.VisualPanel;
+import ball.physics.Dimensions;
+import ball.physics.SpherePos2D;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class VisualTest implements VisualInterface{
+    
+
+    private HeroComponent hero;
+    private List<ArpionComponent> arpions;
+    private final Pair<Integer, Integer> bounds;
+    
+    private VisualPanelTest panel;
+    private ImageLoader iLoader;
+
+
+    private Pos2D startPos;
+
+    public VisualTest(int width, int height, Pos2D startpos){
+        this.bounds = new Pair<Integer,Integer>(width, height);
+
+        this.iLoader = new ImageLoader();
+        this.panel = new VisualPanelTest(width, height, iLoader);
+
+        this.startPos = startpos;
+        this.hero = new HeroComponent(startpos);
+
+        this.arpions = new ArrayList<>(List.of(
+            new ArpionComponent(Color.red, this.hero.getShape().getLeftFoot()), 
+            new ArpionComponent(Color.green, this.hero.getShape().getLeftFoot())
+        ));
+
+    }
+
+    public List<ArpionComponent> getArpions(){
+        return this.arpions;
+    }
+
+    public void move(Pos2D pos){
+        this.hero.changeLocation(pos);
+        this.getArpions().forEach(e -> {
+            if(e.getStatus() == Status.IDLE){
+                e.setLocation(this.hero.getShape().getPos().x, this.hero.getShape().getPos().y);
+            }
+        });
+        Toolkit.getDefaultToolkit().sync();            
+    }
+
+    public void setDirection(Directions dir){
+        this.getArpions().forEach(e -> {
+            if(e.getStatus() == Status.IDLE){
+                e.setDirection(dir, this.hero.getShape());
+            }
+        });
+    }
+
+    public HeroComponent getHeroComponent(){
+        return this.hero;
+    }
+
+    public Pair<Integer, Integer> getBounds(){
+        return this.bounds;
+    }
+
+    public Pos2D getStartPos(){
+        return this.startPos;
+    }
+
+    public Optional<ArpionComponent> getFreeComponent(){
+        for(ArpionComponent i : this.getArpions()){
+            if(i.getStatus() == Status.IDLE){
+                return Optional.of(i);
+            }
+        }
+
+        return Optional.empty();
+
+    }
+
+    public void restoreBullet(ArpionComponent bullet, Pos2D charPos, Directions dir){
+        for(ArpionComponent i : this.getArpions()){
+            if(i == bullet){
+                i.changeLocation(new Pos2D(charPos.x, charPos.y));
+                i.setStatus(Status.IDLE);
+                i.setDirection(dir, this.hero.getShape());
+            }
+        }
+    }
+
+    private Optional<ArpionComponent> giveMeThatArpion(ArpionComponent arpion){
+        for(ArpionComponent i : this.getArpions()){
+            if(i == arpion){
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public void raiseArpion(ArpionComponent arpion){
+        var a = this.giveMeThatArpion(arpion);
+        if(!a.isEmpty()){
+            a.get().raise();
+            Toolkit.getDefaultToolkit().sync();
+        }        
+    }
+
+    public void updatePosition(List<SpherePos2D> pos){
+        panel.updatePositions(pos);
+
+    }
+
+    public int getBallImageDiameter(Dimensions dim){
+        return this.iLoader.getBallImage(dim).getHeight();
+    }
+
+    public VisualPanelTest getVisualTest(){
+        return this.panel;
+    }
+
+
+
+
+    
+
+
+    
+
+}
