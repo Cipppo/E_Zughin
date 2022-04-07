dfe94264ecca10e0bde32720e220150c42f592fb
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-08 00:04:50 +0200

    Implemented simple gui as a test

--- app/src/main/java/mergeballs/gui/GuiTest.java
+++ app/src/main/java/mergeballs/gui/GuiTest.java
@@ -3,25 +3,60 @@ package mergeballs.gui;
 //import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import pangGuy.actions.ActionApplier;
+import pangGuy.character.Hero;
+import pangGuy.utilities.Pair;
+import pangGuy.utilities.Pos2D;
 import stage.utils.*;
 //import stage.components.*;
-
+@SuppressWarnings("unused")
 public class GuiTest extends JFrame{
 
 
-    private final int width = CenterOnDefaultScreen.center().width*70/100;
-    private final int height = CenterOnDefaultScreen.center().height*70/100;
+    private final static int WIDTH = CenterOnDefaultScreen.center().width*70/100;
+    private final static int HEIGHT = CenterOnDefaultScreen.center().height*70/100;
+
+
+    private static final int HERO_WIDTH = 40;
+    private static final int HERO_HEIGHT = 40;
+
+    private static final int STARTX =  WIDTH / 2;
+    private static final int STARTY = (HEIGHT - 35) - HERO_HEIGHT;
+
+
+    private VisualTest visual = new VisualTest(WIDTH, HEIGHT, new Pos2D(STARTX, STARTY));
+    public Hero hero = new Hero();
+    private ActionApplier ap = new ActionApplier(this.visual, this.hero);
+    private JPanel panel = this.visual.getVisualTest();
+
 
 
     public GuiTest(){
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
 
-        this.setSize(width, height);
+        this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
-
         this.setLayout(null);
+
+        this.visual.getArpions().forEach(e -> {
+            this.add(e);
+        });
+        
+        this.add(visual.getHeroComponent());
+
+        this.add(panel);
+
+        System.out.println(WIDTH);
+        System.out.println(HEIGHT);
+
+
+
+
+
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
