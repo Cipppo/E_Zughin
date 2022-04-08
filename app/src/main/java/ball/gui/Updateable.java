5bee8df63a36b6ac6bc46908f2f5ba77d8acf898
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-08 17:35:03 +0200

    interface for handling multiple view frameworks

---
+++ app/src/main/java/ball/gui/Updateable.java
@@ -0,0 +1,16 @@
+package ball.gui;
+
+import ball.physics.SpherePos2D;
+import java.util.List;
+import java.awt.Dimension;
+
+public interface Updateable {
+    /**
+     * Needed to be used from the view, thanks to this
+     * any visual framework only needs to implemnts this.
+     * @param pos
+     */
+    void updatePosition(List<SpherePos2D> pos);
+
+    Dimension getSize();
+}
