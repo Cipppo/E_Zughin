d6f93774e0e47ced3c3c137364010bec249c5241
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-10 22:00:40 +0200

    Implemented PangGuy imageLoader

---
+++ app/src/main/java/pangGuy/gui/PangGuyImageLoader.java
@@ -0,0 +1,56 @@
+package pangGuy.gui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pangGuy.utilities.Directions;
+
+import java.awt.image.BufferedImage;
+import stage.utils.MainImagesLoader;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PangGuyImageLoader {
+
+    private Map<Directions, List<BufferedImage>> entries = new HashMap<>();
+    private MainImagesLoader loader = new MainImagesLoader();
+
+    public PangGuyImageLoader(){
+        this.fillImagesCache(Directions.RIGHT);
+        this.fillImagesCache(Directions.LEFT);
+    }
+
+    private void fillImagesCache(Directions dir){
+        List<String> spriteName = this.getFileNamesGivenDirections(dir);
+        List<BufferedImage> sprites = new ArrayList<>();
+        for(String name : spriteName){
+            sprites.add(loader.load(name));
+        }
+        this.entries.put(dir, sprites);
+    }
+
+    public List<BufferedImage> getHeroImage(Directions dir){
+        return this.entries.get(dir);
+    }
+
+    private List<String> getFileNamesGivenDirections(Directions dir){
+        List<String> temp = new ArrayList<>();
+        switch(dir){
+            case RIGHT:
+                temp.add("StillRight.png");
+                temp.add("WalkingRight1.png");
+                temp.add("WalkinRight2.png");
+                temp.add("PrepShootingRight.png");
+                temp.add("DeathRight.png");
+                break;
+            case LEFT:
+                temp.add("StillLeft.png");
+                temp.add("WalkingLeft1.png");
+                temp.add("WalkinLeft2.png");
+                temp.add("PrepShootingLeft.png");
+                temp.add("DeathLeft.png");
+        }
+        return temp;
+    }
+
+}
