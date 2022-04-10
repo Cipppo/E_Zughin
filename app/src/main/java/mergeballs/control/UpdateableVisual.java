baf8701cd3abbe30e0fb038a259d6d61f3d0e826
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-10 22:46:36 +0200

    Removed unused getArpionsShape()

--- app/src/main/java/mergeballs/control/UpdateableVisual.java
+++ app/src/main/java/mergeballs/control/UpdateableVisual.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 import ball.physics.SpherePos2D;
 import pangGuy.gui.ArpionComponent;
-import pangGuy.gui.Shape;
 import pangGuy.utilities.Pair;
 import pangGuy.utilities.Pos2D;
 
 public interface UpdateableVisual {
-    List<Shape> getArpionsShapes();
-    
     void updatePosition(List<SpherePos2D> pos);
 
     Pair<Integer, Integer> getBounds();
-
-    //Temporary
+    
     List<ArpionComponent> getArpions();
 
     Pos2D getStartPos();
