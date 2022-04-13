f0b07ca732f344f3f6a3c254481b1a82999935fa
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-13 18:24:21 +0200

    first draft of Pickable

---
+++ app/src/main/java/powerUp/Pickable.java
@@ -0,0 +1,36 @@
+package powerUp;
+
+import ball.controller.ConstraintCheck;
+import pangGuy.gui.HeroComponent;
+import pangGuy.gui.Shape;
+
+/**
+ * Class that models a pickable object (Fruit or {@link powerUp.PowerUp}).
+ * A Pickable wraps a {@link pangGuy.gui.Shape} and an instance of Fruit or PowerUp.
+ * This class will be used to handle the moment of picking up this item by
+ * the {@link pangGuy.character.Hero} and activating it's specified effect.
+ */
+public abstract class Pickable {
+    private final Shape shape;
+
+    protected Pickable(Shape shape) {
+        this.shape = shape;
+    }
+   
+    /**
+     * activating the Object wrapped by this class
+     */
+    public abstract void activate();
+    
+    /**
+     * @param hero
+     * @return true if the hero picked this item up
+     */
+    public boolean isPickedUp(HeroComponent hero) {
+        return ConstraintCheck.checkShapeCollsion(this.shape, hero.getShape());
+    }
+    
+    public Shape getShape() {
+        return this.shape;
+    }
+}
