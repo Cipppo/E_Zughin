61e7732325743b6c69ef03a621e820bb31cbd970
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-14 20:48:51 +0200

    Added javadoc

--- app/src/main/java/bird/gui/BirdBoundChecker.java
+++ app/src/main/java/bird/gui/BirdBoundChecker.java
@@ -3,31 +3,61 @@ package bird.gui;
 import pangGuy.utilities.Pair;
 import bird.utilities.BirdPos2D;
 
+/**
+ * This class checks if the position of the bird is out of the stage bounds or not.
+ */
 public class BirdBoundChecker {
 
     private Pair<Integer, Integer> x;
     private Pair<Integer, Integer> y;
 
+    /**
+     * Initializes the bounds with the stage's ones
+     * 
+     * @param x
+     *          abscissas' bounds, lefter and righter.
+     * @param y
+     *          ordinates' bounds, upper and lower.
+     */
     public BirdBoundChecker(Pair<Integer, Integer> x, Pair<Integer, Integer> y){
         this.x = x;
         this.y = y;
     }
 
 
+    /**
+     * Checks if the bird's position is inside the bounds.
+     * 
+     * @param pos
+     *          bird's position.
+     * @param width
+     *          bird's width.
+     * @param height
+     *          birds's height.
+     * @return
+     *          if it's inside or outside.
+     */
     public boolean isInside(BirdPos2D pos ,int width, int height){
         if(pos.x >= this.x.getX() && pos.x + width <= this.x.getY()){
             if(pos.y >= this.y.getX() && pos.y + height <= this.y.getY() - 35){
-                //System.out.println("PERFORMED");
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * @return
+     *          the abscissa's pair.
+     */
     public Pair<Integer, Integer> getXPair() {
         return this.x;
     }
 
+    /**
+     * @return
+     *          the ordinate's pair.
+     */
     public Pair<Integer, Integer> getYPair() {
         return this.y;
     }
--- app/src/main/java/bird/gui/BirdMover.java
+++ app/src/main/java/bird/gui/BirdMover.java
@@ -3,16 +3,33 @@ package bird.gui;
 import bird.utilities.BirdDirections;
 import bird.utilities.BirdPos2D;
 
+/**
+ * Checks if the bird is inside the stage and than moves it.
+ */
 public class BirdMover {
     
     private final BirdActor actor;
     private final BirdBoundChecker bc;
 
+    /**
+     * Constructor that initializes the bird and the bounds checker.
+     * @param actor
+     *          bird.
+     * @param bc
+     *          bound checker.
+     */
     public BirdMover(BirdActor actor, BirdBoundChecker bc) {
         this.actor = actor;
         this.bc = bc;
     }
 
+    /**
+     * Moves the bird to the desired position.
+     * @param pos
+     *          position to move the bird.
+     * @param dir
+     *          direction the bird is flying.
+     */
     public void move(BirdPos2D pos, BirdDirections dir) {
         if(bc.isInside(pos, this.actor.getShape().getDimensions().getX(), 
         this.actor.getShape().getDimensions().getY())) {
@@ -20,6 +37,10 @@ public class BirdMover {
         }
     }
 
+    /**
+     * @return
+     *          the current position of the bird
+     */
     public BirdPos2D getCurrentPos(){
         return this.actor.getShape().getPos();
     }
