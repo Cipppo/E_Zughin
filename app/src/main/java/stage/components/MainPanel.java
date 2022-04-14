f0213426c631e2982a6ae48805bdd4923e6acc66
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-14 14:03:10 +0200

    Added runner to staage main panel

--- app/src/main/java/stage/components/MainPanel.java
+++ app/src/main/java/stage/components/MainPanel.java
@@ -3,6 +3,8 @@ package stage.components;
 import javax.swing.JPanel;
 import java.awt.CardLayout;
 
+import bird.controller.BirdConstraintCheck;
+import bird.controller.BirdRunner;
 import bird.utilities.BirdVisual;
 import pauseMenu.components.ExitButton;
 import pauseMenu.components.PauseButton;
@@ -18,6 +20,8 @@ public class MainPanel extends JPanel{
 	private static final long serialVersionUID = -5567586066880648121L;
 	private PausePanel pausePanel = new PausePanel();
 	private final BirdVisual visual = new BirdVisual(this);
+	private final BirdConstraintCheck checker = new BirdConstraintCheck();
+	private final BirdRunner runner = new BirdRunner(this, checker);
 
 	
 	public MainPanel() {
@@ -27,8 +31,8 @@ public class MainPanel extends JPanel{
 		new ExitButton(this, pausePanel);
 	}
 
-	public BirdVisual getVisual() {
-		return this.visual;
+	public BirdRunner getBirdRunner() {
+		return this.runner;
 	}
 
 }
