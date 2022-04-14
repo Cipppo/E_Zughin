412f5874f359a0fc5622cd7f8ded65cce641d251
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-14 15:32:47 +0200

    Refactoring from getVisual to getBirdRunner

--- app/src/main/java/stage/gui/Gui.java
+++ app/src/main/java/stage/gui/Gui.java
@@ -29,7 +29,7 @@ public class Gui extends JFrame{
 		this.add(new HUDPanel(), BorderLayout.SOUTH);
 		this.add(mainPanel, BorderLayout.CENTER);
 		this.setVisible(true);
-		mainPanel.getVisual().run();
+		mainPanel.getBirdRunner().run();
 	}
 	
 }
