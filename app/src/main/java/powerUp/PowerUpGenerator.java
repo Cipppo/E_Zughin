99005b538cb0ec79686acf805701b739cc8342ef
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-14 10:22:12 +0200

    Implemented powerupGenerator in order to generate random powerups

--- app/src/main/java/powerUp/PowerUpGenerator.java
+++ app/src/main/java/powerUp/PowerUpGenerator.java
@@ -2,20 +2,33 @@ package powerUp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import ball.controller.Runner;
 import pangGuy.modularGun.GunSet;
 
 public class PowerUpGenerator {
 
-    List<PowerUp> powerups = new ArrayList<>();
+    private List<PowerUp> powerups;
 
-    public PowerUpGenerator(GunSet gSet){
-
-        this.powerups.add(
-            new StickyArpion(gSet);
-        )
-
+    public PowerUpGenerator(GunSet gSet, Runner context){
+        this.powerups = new ArrayList<>(List.of(
+            new StickyArpion(gSet), 
+            new DoubleGun(gSet), 
+            new Bomb(context), 
+            new TimeFreeze(context)
+        ));
     }
+
+    private int getRandomInt(){
+        Random rand = new Random();
+        return rand.nextInt(5);
+    }
+
+    public PowerUp getRandomPowerUp(){
+        return this.powerups.get(this.getRandomInt());
+    }
+
 
 
 }
