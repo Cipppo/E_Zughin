c3d3ae389f326f697437c0e1ba7b1394c64a7770
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-14 11:13:26 +0200

    Insert PowerupImageLoader

--- app/src/main/java/mergeballs/gui/VisualPanelTest.java
+++ app/src/main/java/mergeballs/gui/VisualPanelTest.java
@@ -7,6 +7,7 @@ import java.awt.Toolkit;
 import java.awt.Graphics2D;
 
 import java.util.List;
+import java.util.Optional;
 
 import ball.gui.ImageLoader;
 import ball.physics.SpherePos2D;
@@ -16,6 +17,8 @@ import pangGuy.gui.PangGuyImageLoader;
 import pangGuy.gui.Shape;
 import pangGuy.modularGun.GunTypes;
 import pangGuy.utilities.Directions;
+import powerUp.PowerUpEntity;
+import powerUp.PowerupImageLoader;
 
 public class VisualPanelTest extends JPanel {
 
@@ -29,14 +32,17 @@ public class VisualPanelTest extends JPanel {
     private final ImageLoader iLoader;
     private final ArpionImageLoader aILoader;
     private final PangGuyImageLoader heroILoader;
+    private final PowerupImageLoader pUpImageLoader;
+    private Optional<PowerUpEntity> pUp;
 
-    public VisualPanelTest(int width, int height, ImageLoader iLoader, PangGuyImageLoader heroILoader, ArpionImageLoader aILoader) {
+    public VisualPanelTest(int width, int height, ImageLoader iLoader, PangGuyImageLoader heroILoader, ArpionImageLoader aILoader, PowerupImageLoader pIl) {
         super.setSize(width, height);
         this.width = width;
         this.height = height;
         this.iLoader = iLoader;
         this.heroILoader = heroILoader;
         this.aILoader = aILoader;
+        this.pUpImageLoader = pIl;
     }
 
     public void paint(Graphics g) {
@@ -59,17 +65,22 @@ public class VisualPanelTest extends JPanel {
             if (this.hShape != null) {
                 g2.drawImage(heroILoader.getHeroImage(this.dir).get(0), this.hShape.getPos().x, this.hShape.getPos().y, this);
             }
+            if(!this.pUp.isEmpty()){
+                g2.drawImage(pUpImageLoader.getPowUpSprite(this.pUp.get().getPowerUp()).get(), this.pUp.get().getShape().getPos().x, this.pUp.get().getShape().getPos().y, this);
+                System.out.println("x" + pUp.get().getShape().getPos().x + "y" + pUp.get().getShape().getPos().x);
+            }
         }
         g2.dispose();
         Toolkit.getDefaultToolkit().sync();
     }
 
-    public void updatePositions(List<SpherePos2D> pos, List<Shape> aShapes, Shape hShape, Directions dir, GunTypes type) {
+    public void updatePositions(List<SpherePos2D> pos, List<Shape> aShapes, Shape hShape, Directions dir, GunTypes type, Optional<PowerUpEntity> pUp) {
         ballPositions = pos;
         shapes = aShapes;
         this.hShape = hShape;
         this.dir = dir;
         this.gunType = type;
+        this.pUp = pUp;
         repaint();
     }
 
