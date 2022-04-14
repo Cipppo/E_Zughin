f704fb7f377c2b552d41f683fada163d163b2b0a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-14 10:21:35 +0200

    Create simple Powerup Controller

---
+++ app/src/main/java/powerUp/PowerUpHandler.java
@@ -0,0 +1,65 @@
+package powerUp;
+
+import java.util.Optional;
+import java.util.Random;
+
+import ball.controller.Runner;
+import pangGuy.gui.Shape;
+import pangGuy.modularGun.GunSet;
+import pangGuy.utilities.Pair;
+import pangGuy.utilities.Pos2D;
+
+public class PowerUpHandler extends Thread{
+
+    private static final int POWERUP_WIDTH = 17;
+    private static final int POWERUP_HEIGHT = 17;
+    private static final int SPAWN_TIME = 5;
+
+    private final PowerUpGenerator gen;
+    private boolean stop;
+    Optional<PowerUpEntity> next;
+    private Pair<Integer, Integer> bounds;
+
+    public PowerUpHandler(GunSet gSet, Runner context, Pair<Integer, Integer> bounds){
+        this.gen = new PowerUpGenerator(gSet, context);
+        this.stop = false;
+        this.next = Optional.empty();
+        this.bounds = bounds;
+    }
+
+    @Override
+    public void run(){
+        while(!stop){
+            try {
+                this.next = Optional.of(new PowerUpEntity(this.gen.getRandomPowerUp(), this.generateRandomShape()));
+                Thread.sleep(SPAWN_TIME * 1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread.sleep() error: " + e.getMessage());
+            }
+        }
+    }
+
+    private Pos2D getRandomPos2D(){
+        Random rand = new Random();
+        return new Pos2D(rand.nextInt(this.bounds.getX()), this.bounds.getY());
+    }
+
+    private Shape generateRandomShape(){
+        
+        return new Shape(this.getRandomPos2D(), POWERUP_WIDTH, POWERUP_HEIGHT);
+    }
+
+    public Optional<PowerUpEntity> getPowerup(){
+        if(!this.next.isEmpty()){
+            var buff = this.next;
+            this.next = Optional.empty();
+            return buff;
+        }
+        return Optional.empty();
+    }    
+
+    public void terminate(){
+        this.stop = true;
+    }
+    
+}
