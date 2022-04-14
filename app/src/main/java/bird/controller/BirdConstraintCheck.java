9454b3324e3870b095b8377965c53c4aabd1279e
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-14 13:38:13 +0200

    Created Bird constraint check

---
+++ app/src/main/java/bird/controller/BirdConstraintCheck.java
@@ -0,0 +1,40 @@
+package bird.controller;
+
+import ball.utils.Pair;
+import bird.gui.BirdActor;
+import bird.gui.BirdShape;
+import pangGuy.gui.Shape;
+
+public class BirdConstraintCheck {
+
+    public boolean checkEnemyCollision(Shape entity, BirdActor bird) {
+        return isCollision(bird.getShape(), entity);
+    }
+
+    private boolean isCollision(BirdShape bird, Shape rect) {
+        var rectWidht = rect.getDimensions().getX();
+        var rectHeight = rect.getDimensions().getY();
+        Pair<Integer> rectCenter = new Pair<Integer>(rect.getPos().x + (int)(0.5*rectWidht),
+                                                    rect.getPos().y + (int)(0.5*rectHeight));
+        Pair<Integer> birdCenter = new Pair<Integer>(bird.getPos().x + (bird.getDimensions().getX() / 2),
+                                                    bird.getPos().y + (bird.getDimensions().getY() / 2));
+        Pair<Integer> birdDistance = new Pair<Integer>(0, 0);
+
+        birdDistance.setX(Math.abs(birdCenter.getX() - rectCenter.getY()));
+        birdDistance.setY(Math.abs(birdCenter.getY() - rectCenter.getY()));
+
+        if(birdDistance.getX() > (rectWidht / + bird.getDimensions().getX() / 2)) {
+            return false;
+        }
+
+        if(birdDistance.getY() > (rectHeight / 2 + bird.getDimensions().getY() / 2)) {
+            return false;
+        }
+
+        if(birdDistance.getX() <= (rectWidht / 2) || birdDistance.getY() <= (rectHeight / 2)) {
+            return true;
+        }
+
+        return false;
+    }
+}
