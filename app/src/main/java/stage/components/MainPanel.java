30e74998e0b91df971d0df53f1bad374ec58903a
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-15 14:06:07 +0200

    deleted unesd class

--- app/src/main/java/stage/components/MainPanel.java
+++ app/src/main/java/stage/components/MainPanel.java
@@ -4,8 +4,7 @@ import javax.swing.JPanel;
 
 import java.awt.CardLayout;
 
-import bird.controller.BirdConstraintCheck;
-import bird.controller.BirdRunner;
+import bird.utilities.BirdVisual;
 import pauseMenu.components.ExitButton;
 import pauseMenu.components.PauseButton;
 import pauseMenu.components.PausePanel;
@@ -19,20 +18,19 @@ public class MainPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = -5567586066880648121L;
 	private PausePanel pausePanel = new PausePanel();
-	private final BirdConstraintCheck checker = new BirdConstraintCheck();
-	private final BirdRunner runner = new BirdRunner(this, checker);
+	private final BirdVisual bird = new BirdVisual(this);
 
 	
 	public MainPanel() {
 		this.setBackground(Color.WHITE);
 		this.setLayout(new CardLayout());
-		new PauseButton(this, pausePanel, this.runner.getBirdVisual());
+		new PauseButton(this, pausePanel, this.bird);
 		new ExitButton(this, pausePanel);
 		this.setVisible(true);
 	}
 
-	public BirdRunner getBirdRunner() {
-		return this.runner;
+	public BirdVisual getBird() {
+		return this.bird;
 	}
 
 }
