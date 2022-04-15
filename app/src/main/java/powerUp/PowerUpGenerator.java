215e4aef4235fdaa28c8630207dcf45d69000486
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-15 18:50:47 +0200

    horrible and temporary fix of spawning always new powerups

--- app/src/main/java/powerUp/PowerUpGenerator.java
+++ app/src/main/java/powerUp/PowerUpGenerator.java
@@ -1,23 +1,17 @@
 package powerUp;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import ball.controller.Runner;
 import pangGuy.modularGun.GunSet;
 
 public class PowerUpGenerator {
-
-    private List<PowerUp> powerups;
+    private GunSet gSet;
+    private Runner context;
 
     public PowerUpGenerator(GunSet gSet, Runner context){
-        this.powerups = new ArrayList<>(List.of(
-            new StickyArpion(gSet), 
-            new DoubleGun(gSet), 
-            new Bomb(context), 
-            new TimeFreeze(context)
-        ));
+        this.gSet = gSet;
+        this.context = context;
     }
 
     private int getRandomInt(){
@@ -25,10 +19,19 @@ public class PowerUpGenerator {
         return rand.nextInt(4);
     }
 
-    public PowerUp getRandomPowerUp(){
-        return this.powerups.get(this.getRandomInt());
+    public PowerUp getRandomPowerUp() {
+        var num = this.getRandomInt();
+        switch (num) {
+            case 0:
+                return new StickyArpion(this.gSet);
+            case 1:
+                return new DoubleGun(this.gSet);
+            case 2:
+                return new Bomb(context);
+            case 3:
+                return new TimeFreeze(context);
+            default:
+                return null;
+        }
     }
-
-
-
 }
