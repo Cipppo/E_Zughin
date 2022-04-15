556308169b3c2955047c78fe8bd78a58c9fc8929
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-15 18:50:01 +0200

    added activation checks

--- app/src/main/java/powerUp/PowerUpEntity.java
+++ app/src/main/java/powerUp/PowerUpEntity.java
@@ -4,15 +4,20 @@ import pangGuy.gui.Shape;
 
 public class PowerUpEntity extends Pickable {
     private final PowerUp powerUp;
+    private boolean isActivated;
 
     public PowerUpEntity(PowerUp powerUp, Shape shape) {
         super(shape);
         this.powerUp = powerUp;
+        this.isActivated = false;
     }
 
     @Override
     public void activate() {
-        this.powerUp.activate();
+        if (!this.isActivated) {
+            this.isActivated = true;
+            this.powerUp.activate();
+        }
     }
 
     public PowerUp getPowerUp(){
