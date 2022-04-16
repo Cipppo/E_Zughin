b77c128d57e9b2badba9611c95979aff14935133
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 17:53:10 +0200

    Add javadoc

--- app/src/main/java/pangGuy/gui/HeroComponent.java
+++ app/src/main/java/pangGuy/gui/HeroComponent.java
@@ -3,6 +3,10 @@ package pangGuy.gui;
 
 import pangGuy.utilities.Pos2D;
 
+/**
+ * This class models a character in the space and keeps track of his position
+ * in the space.
+ */
 
 public class HeroComponent {
 
@@ -11,19 +15,29 @@ public class HeroComponent {
 
     private Shape s;
 
-
-
+    /**
+     * Create a new HeroComponent (with predetermined WIDTH and HEIGHT) 
+     * given the startPosition in the GUI.
+     * @param startPos
+     */
     public HeroComponent(Pos2D startPos){
         this.s = new Shape(startPos, WIDTH, HEIGHT);
 
     }
 
-
+    /**
+     * By giving an EntityPosition, it updates the EntityShape.
+     * @param pos
+     */
     public void changeLocation(Pos2D pos){
         this.s = new Shape(pos, WIDTH, HEIGHT);
 
     }
 
+    /**
+     * Getter for the EntityShape.
+     * @return the actual EntityShape.
+     */
     public Shape getShape(){
         return this.s;
     }
