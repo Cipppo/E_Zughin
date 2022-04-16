ea80f8fc6bbac597b214b3e30d12a095f730b1b4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 21:05:54 +0200

    Changed imports

--- app/src/main/java/ball/testing/SquaredEnemy.java
+++ app/src/main/java/ball/testing/SquaredEnemy.java
@@ -1,21 +1,21 @@
 package ball.testing;
 
 import pangGuy.gui.Shape;
-import pangGuy.utilities.Pair;
-import pangGuy.utilities.Pos2D;
+import mergeballs.utilities.EntityPos2D;
+import mergeballs.utilities.FullPair;
 
 public class SquaredEnemy{
     private Shape shape;
 
     public SquaredEnemy(int x, int y, int width, int height) {
-        this.shape = new Shape(new Pos2D(x, y), width, height);
+        this.shape = new Shape(new EntityPos2D(x, y), width, height);
     }
 
-    public Pos2D getPosition() {
+    public EntityPos2D getPosition() {
         return this.shape.getPos();
     }
 
-    public Pair<Integer, Integer> getDimension() {
+    public FullPair<Integer, Integer> getDimension() {
         return this.shape.getDimensions();
     }
 
