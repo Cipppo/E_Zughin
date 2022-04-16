6f8cb9b2d5934f5155ded49e2c9f9865884812d0
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 18:00:56 +0200

    Add javadoc

--- app/src/main/java/pangGuy/gui/PangGuyImageLoader.java
+++ app/src/main/java/pangGuy/gui/PangGuyImageLoader.java
@@ -10,16 +10,27 @@ import stage.utils.MainImagesLoader;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * This class manages the Hero character sprites.
+ */
+
 public class PangGuyImageLoader {
 
     private Map<Directions, List<BufferedImage>> entries = new HashMap<>();
     private MainImagesLoader loader = new MainImagesLoader();
 
+    /**
+     * Create a new PangGuyImageLoader with a cache full of sprites sorted by their directions.
+     */
     public PangGuyImageLoader(){
         this.fillImagesCache(Directions.RIGHT);
         this.fillImagesCache(Directions.LEFT);
     }
 
+    /**
+     * Fills the cache with the selected sprites.
+     * @param dir the choosen direction.
+     */
     private void fillImagesCache(Directions dir){
         List<String> spriteName = this.getFileNamesGivenDirections(dir);
         List<BufferedImage> sprites = new ArrayList<>();
@@ -29,11 +40,20 @@ public class PangGuyImageLoader {
         this.entries.put(dir, sprites);
     }
 
+    /**
+     * Returns a List of sprites having the same directions.
+     * @param dir the choosen direction.
+     * @return a List full of sprites.
+     */
     public List<BufferedImage> getHeroImage(Directions dir){
-       
         return this.entries.get(dir);
     }
 
+    /**
+     * Given a Directions it returns a List of file names.
+     * @param dir the choosen direction.
+     * @return a List full of image names.
+     */
     private List<String> getFileNamesGivenDirections(Directions dir){
         List<String> temp = new ArrayList<>();
         switch(dir){
