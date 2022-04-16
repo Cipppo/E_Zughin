18efb5950968b88d04bf83e5b54ec822314b7ed9
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 18:57:11 +0200

    Refactor and add Javadoc

--- app/src/main/java/pangGuy/character/Hero.java
+++ app/src/main/java/pangGuy/character/Hero.java
@@ -3,58 +3,88 @@ package pangGuy.character;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import mergeballs.utilities.EntityPos2D;
 import pangGuy.modularGun.GunSet;
 
 import pangGuy.utilities.Directions;
-import pangGuy.utilities.Pos2D;
+
+/**
+ * Models the model of the main character.
+ */
 
 public class Hero {
     
     private static final int HIT_WAIT = 1;
 
-    private Pos2D pos;
+    private EntityPos2D pos;
     private Directions direction;
     private GunSet gSet;
     private heroStatus status;
     private int lives;
 
+    /**
+     * Creates the main character giving default values.
+     */
     public Hero(){
-        this.pos = new Pos2D(0, 0);
+        this.pos = new EntityPos2D(0, 0);
         this.direction = Directions.LEFT;
         this.gSet = new GunSet();
         this.status = heroStatus.NEUTRAL;
         this.lives = 3;
     }
 
+    /**
+     * Updates the hero direction.
+     * @param dir the given direction.
+     */
     private void updateDirection(Directions dir){
         this.direction = dir;
         this.gSet.setDirections(dir);
     }
 
+    /**
+     * Getter for the actual Direction.
+     * @return the actual direction.
+     */
     public Directions getDirection(){
         return this.direction;
     }
 
-    public Pos2D getPosition(){
+    /**
+     * Getter for the actual absolute position.
+     * @return the absolute position.
+     */
+    public EntityPos2D getPosition(){
         return this.pos;
     }
 
+    /**
+     * Giving a Directions, it change the absolute coords making a step.
+     * @param dir the direction where it has to step.
+     */
     public void move(Directions dir){
         switch (dir) {
             case RIGHT:
-                this.pos = new Pos2D(pos.x + 1, pos.y);
+                this.pos = new EntityPos2D(pos.x + 1, pos.y);
                 break;
             case LEFT:
-                this.pos = new Pos2D(pos.x - 1, pos.y);
+                this.pos = new EntityPos2D(pos.x - 1, pos.y);
                 break;
         }
         this.updateDirection(dir);
     }
-
+    
+    /**
+     * Getter for the GunSet.
+     * @return the GunSet in use.
+     */
     public GunSet getGset(){
         return this.gSet;
     }
 
+    /**
+     * The method is called whenever a ball hits the main character, it cut off one life.
+     */
     public void hit(){
         if(this.status == heroStatus.NEUTRAL){
             this.status = heroStatus.HIT;
