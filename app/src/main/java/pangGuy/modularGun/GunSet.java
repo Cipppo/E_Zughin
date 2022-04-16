9e5777f2d6c4235b02be8530683d01a5a9f9c80f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 20:06:07 +0200

    Refactor and add JavaDoc

--- app/src/main/java/pangGuy/modularGun/GunSet.java
+++ app/src/main/java/pangGuy/modularGun/GunSet.java
@@ -1,33 +1,35 @@
 package pangGuy.modularGun;
 
-
-
-
 import java.util.Set;
-
 import java.util.List;
 import java.util.ArrayList;
-
 import java.util.Optional;
 import pangGuy.utilities.Directions;
 
+
+/**Models a set of two guns, where the second one is usable only if
+ * DoubleArpion powerup is picked up.
+ */
+
 public class GunSet{
 
-    //private final static long WAIT = 2000;
-
     private final List<Bullet> arpions;
-    
-
     private GunTypes currentGun;
 
+    /**
+     * Creates a new GunSet with default values.
+     */
     public GunSet(){
         this.arpions = new ArrayList<Bullet>(
             Set.of(new Arpion(), new Arpion())
         );
-
         this.currentGun = GunTypes.ARPION;
     }
-
+    
+    /**
+     * Gives an arpion checking the current powerup in action.
+     * @return Optional<Bullet> if there is a free gun, Optional.empty() otherwise.
+     */
     private Optional<Bullet> getSingleArpion(){
         if(this.currentGun == GunTypes.ARPION || this.currentGun == GunTypes.STICKY_ARPION){
             if(this.arpions.get(0).getStatus() == Status.IDLE){
@@ -45,17 +47,28 @@ public class GunSet{
         return Optional.empty();
     }
 
+    /**
+     * Setter for the GunType.
+     * @param type the GunType which needs to be set.
+     */
     public void setGunType(GunTypes type){
         this.currentGun = type;
     }
 
+    /**
+     * Resets the GunType only if the caller is the actual gunType (Like a sort of mutex).
+     * @param caller the GunType which asks to be resetted.
+     */
     public void resetGunType(GunTypes caller){
         if(caller == this.currentGun){
             this.currentGun = GunTypes.ARPION;
         }
     }
 
-
+    /**
+     * 
+     * @return Optional<Bullet> if there is a gun available, Optional.empty otherwise.
+     */
     public Optional<Bullet> getShootingGun(){
         Optional<Bullet> shootingGun = Optional.empty();
     
@@ -66,7 +79,10 @@ public class GunSet{
         return shootingGun;       
     }
 
-
+    /**
+     * Sets the Arpion directions in order to lay in the right coords.
+     * @param dir the Direction which needs to be set.
+     */
     public void setDirections(Directions dir){
         for(Bullet i : this.arpions){
             i.changeDir(dir);
@@ -74,22 +90,38 @@ public class GunSet{
         }
     }
 
+    /**
+     * 
+     * @return a List of the two Arpions.
+     */
     public List<Bullet> getArpions(){
         return this.arpions;
     }
 
+    /**
+     * Set the Arpions wait time between the Y_MAX point reach and restore.
+     * @param wait the seconds to be wait.
+     */
     public void setWaitTime(int wait){
         this.getArpions().forEach(e -> {
             e.setWaitTime(wait);
         });
     }
 
+    /**
+     * Resets the Arpions wait time.
+     */
     public void resetWaitTime(){
         this.getArpions().forEach(e ->{
             e.setWaitTime(0);
         });
     }
 
+    /**
+     * Given a Step number, it searches the Arpion which has made the steps.
+     * @param steps the Steps which the Arpion Made.
+     * @return Optional<Bullet> if it finds the arpion, Optional.empty otherwhise.
+     */
     public Optional<Bullet> getBulletFromSteps(int steps){
         for(Bullet i : this.arpions){
             if(i.getStepsDone() == steps){
@@ -99,6 +131,10 @@ public class GunSet{
         return Optional.empty();
     }
 
+    /**
+     * Getter fot the GunType
+     * @return the current GunType
+     */
     public GunTypes getGunType(){
         return this.currentGun;
     }
