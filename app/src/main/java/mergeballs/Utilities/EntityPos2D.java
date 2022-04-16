7317a0b662deaf530b121b0e80da7688ddef0206
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 17:41:00 +0200

    Add javadoc

--- app/src/main/java/mergeballs/Utilities/EntityPos2D.java
+++ app/src/main/java/mergeballs/Utilities/EntityPos2D.java
@@ -1,15 +1,28 @@
 package mergeballs.utilities;
 
+/**
+ * Simple POJO class useful to store 2 dimensions positions.
+ */
+
+
 public class EntityPos2D {
     
     public int x;
     public int y;
 
+    /**
+     * Create a new Point in the field.
+     * @param x as the X position 
+     * @param y as the Y position
+     */
     public EntityPos2D(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return a string containing the X and Y coords.
+     */
     @Override
     public String toString(){
         return "(x = " + this.x + "| y = " + this.y + ")" ;
--- app/src/main/java/mergeballs/Utilities/EntityShape.java
+++ app/src/main/java/mergeballs/Utilities/EntityShape.java
@@ -1,9 +1,22 @@
 package mergeballs.utilities;
 
+/**
+ * Models a useful class which can store a component position and his dimensions in the space.
+ */
+
+
 public interface EntityShape {
 
+    /**
+     * Getter for the position in space.
+     * @return the position in the space under EntityPos2D.
+     */
     EntityPos2D getPos();
     
+    /**
+     * Getter for the component dimensions in space.
+     * @return a FullPair<Width, Height>
+     */
     FullPair<Integer, Integer> getDimensions();
 
 
