9016a1d8c56bda24ce394b9202021461678c7ef1
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 19:26:14 +0200

    Refactor and add Javadoc

--- app/src/main/java/pangGuy/modularGun/Trigger.java
+++ app/src/main/java/pangGuy/modularGun/Trigger.java
@@ -4,8 +4,14 @@ import pangGuy.character.Hero;
 import pangGuy.gui.ArpionComponent;
 import pangGuy.gui.BoundChecker;
 import mergeballs.gui.VisualInterface;
+import mergeballs.utilities.EntityPos2D;
 import pangGuy.utilities.Pos2D;
 import pangGuy.utilities.StepsApplier;
+
+/**
+ * Manages the Rising of the Arpions.
+ */
+
 
 public class Trigger extends Thread{
     
@@ -15,9 +21,16 @@ public class Trigger extends Thread{
     private BoundChecker bc;
     private StepsApplier stepConverter;
     private Hero hero;
-
     private boolean stop = false;
 
+    /**
+     * Creates a new Trigger.
+     * @param arpion the Model of the Arpion which is going to be raised.
+     * @param arpionComponent the Component of the Arpion which is gonna be raised.
+     * @param visual the Visual in order to retrieve the start GUI positon.
+     * @param bc the boundChecker in order to stop the arpion at a certain time.
+     * @param hero the hero Model.
+     */
     public Trigger(Bullet arpion, ArpionComponent arpionComponent, VisualInterface visual, BoundChecker bc, Hero hero){
         this.arpion = arpion;
         this.arpionComponent = arpionComponent;
@@ -28,29 +41,26 @@ public class Trigger extends Thread{
     }
 
     @Override
+    /**
+     * It manages the rising of the Arpion by making steps and checking if the next position is eligible.
+     */
     public void run(){
         int xValue = this.stepConverter.convertHeroPosition(this.hero.getPosition()).x;
         this.arpion.lock(xValue);
         this.arpionComponent.setStatus(this.arpion.getStatus());
         this.arpionComponent.setType(this.hero.getGset().getGunType());
-        
         int yvalue = this.stepConverter.covertStepPosition(this.arpion.getStepsDone());
         int maxYValue = this.visual.getBounds().getY();
-        
         try{
             while(!this.stop){
                     this.arpion.raise();
                     this.visual.raiseArpion(arpionComponent);
-
-                    
-                    
                     if(!this.bc.isExtendible(new Pos2D(xValue, maxYValue - this.stepConverter.covertStepPosition(this.arpion.getStepsDone()))) || 
                             this.arpion.getStatus() == Status.HIT){
                         this.stop = true;
                     }
                     Thread.sleep(8);
             }
-
             if(this.arpion.getWaitTime() > 0){
                 int sync = 0;
                 if(this.arpion.getWaitTime() > 0){
@@ -63,11 +73,8 @@ public class Trigger extends Thread{
                     }
                 }
             }
-
             this.arpion.restore();
-            this.visual.restoreBullet(arpionComponent, new Pos2D(stepConverter.convertHeroPosition(this.hero.getPosition()).x, maxYValue - yvalue), this.hero.getDirection());
-            
-            
+            this.visual.restoreBullet(arpionComponent, new EntityPos2D(stepConverter.convertHeroPosition(this.hero.getPosition()).x, maxYValue - yvalue), this.hero.getDirection());
         }catch(Exception e){
             System.out.println("An error has occured: " + e.getMessage());
         }
