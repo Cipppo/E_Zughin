0c25ba8fefa39e7b07a8c2e8e43b87795c7ec367
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 18:24:49 +0200

    Implemented EntityShape and add javadoc

--- app/src/main/java/pangGuy/gui/Shape.java
+++ app/src/main/java/pangGuy/gui/Shape.java
@@ -2,35 +2,57 @@ package pangGuy.gui;
 
 import java.awt.Rectangle;
 
-import pangGuy.utilities.Pos2D;
+import mergeballs.utilities.EntityShape;
+import mergeballs.utilities.EntityPos2D;
+import mergeballs.utilities.FullPair;
 import pangGuy.utilities.Pair;
 
-public class Shape {
+/**
+ * It stores the position in a 2D space of a 2D entity.
+ */
+
+
+public class Shape implements EntityShape{
 
     private final Rectangle r;
 
-
-    public Shape(Pos2D pos, int width, int height){
+    /**
+     * Create a new Shape.
+     * @param pos the position in the 2D space.
+     * @param width the width of the entity.
+     * @param height the height of the entity.
+     */
+    public Shape(EntityPos2D pos, int width, int height){
         r = new Rectangle(pos.x, pos.y, width, height);
     }
     
-
-    public Pos2D getPos(){
-        return new Pos2D((int)r.getX(), (int) r.getY());
+    @Override
+    public EntityPos2D getPos(){
+        return new EntityPos2D((int)r.getX(), (int) r.getY());
     }
 
-
-    public Pair<Integer, Integer> getDimensions(){
+    @Override
+    public FullPair<Integer, Integer> getDimensions(){
         return new Pair<Integer, Integer>((int)r.getWidth(), (int)r.getHeight());
     }
     
+    /**
+     * Getter for the Rectangle made from the entity specs.
+     * @return Rectangle made from the entity specs.
+     */
     public Rectangle getRectangle(){
         return this.r;
     }
 
-    public Pos2D getLeftFoot(){
-        Pos2D mainPos = this.getPos();
-        return new Pos2D(mainPos.x, mainPos.y + (int) this.r.getHeight());
+    /**
+     * Assuming that the "head" of the rectangle is the up-left corner, 
+     * it returns the down-left corner (Left foot).
+     * The method is called in order to position the arpions.
+     * @return an EntityPos2D with the left foot coords.
+     */
+    public EntityPos2D getLeftFoot(){
+        EntityPos2D mainPos = this.getPos();
+        return new EntityPos2D(mainPos.x, mainPos.y + (int) this.r.getHeight());
     }
 
     @Override
