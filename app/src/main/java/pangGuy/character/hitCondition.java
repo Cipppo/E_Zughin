b5fe0de557495345ddc2044676a7848fa6d64717
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 20:58:28 +0200

    Add Javadoc

--- app/src/main/java/pangGuy/character/hitCondition.java
+++ app/src/main/java/pangGuy/character/hitCondition.java
@@ -2,10 +2,15 @@ package pangGuy.character;
 
 import java.util.TimerTask;
 
+/**
+ * Models a TimerTask used to set the Hero status to NEUTRAL after certain time.
+ */
+
 public class hitCondition extends TimerTask{
 
     private heroStatus status;
 
+    
     public hitCondition(heroStatus status){
         this.status = status;
     }
