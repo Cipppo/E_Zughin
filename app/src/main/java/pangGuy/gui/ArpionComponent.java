3c0ee4c3f0b3931d1a0e83ec4f821855b45d20c2
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-16 20:47:07 +0200

    Refactor and add Javadoc

--- app/src/main/java/pangGuy/gui/ArpionComponent.java
+++ app/src/main/java/pangGuy/gui/ArpionComponent.java
@@ -5,15 +5,15 @@ import pangGuy.modularGun.GunTypes;
 import pangGuy.modularGun.Status;
 import pangGuy.utilities.Directions;
 import pangGuy.utilities.Pair;
-import pangGuy.utilities.Pos2D;
 import pangGuy.utilities.PosConverter;
-
-
 import java.awt.Color;
+import mergeballs.utilities.EntityPos2D;
 
-import javax.swing.JPanel;
+/**
+ * Models a GUI component for the arpion class.
+ */
 
-public class ArpionComponent extends JPanel{
+public class ArpionComponent{
     
     private static final int WIDTH = 10;
     private static final int HEIGHT = 2000;
@@ -24,54 +24,86 @@ public class ArpionComponent extends JPanel{
     private Directions dir;
     private GunTypes type;
 
-    public ArpionComponent(Color color, Pos2D startpos){
+    /**
+     * Creates a new ArpionComponent with default values
+     * @param startpos the position where the component has to be set in the GUI at the start.
+     */
+    public ArpionComponent(EntityPos2D startpos){
         this.s = new Shape(startpos, WIDTH, HEIGHT);
         this.status = Status.IDLE;
-
-        super.setBackground(color);
-        super.setForeground(color);
-        super.setBounds(this.s.getRectangle());
-        super.setOpaque(true);
         this.dir = Directions.LEFT;
         this.type = GunTypes.ARPION;
-
     }
 
-    public void changeLocation(Pos2D pos){
-        super.setLocation(pos.x, pos.y);
+    /**
+     * Setter for the Position.
+     * @param pos the next component position.
+     */
+    public void changeLocation(EntityPos2D pos){
         this.s = new Shape(pos, WIDTH, HEIGHT);
     }
 
+    /**
+     * Getter for the Shape.
+     * @returnthe the actual Shape.
+     */
     public Shape getShape(){
         return this.s;
     }
 
+    /**
+     * Changes the position based on the direction. 
+     * @param dir the next direction.
+     * @param shape the Hero Shape.
+     */
     public void setDirection(Directions dir, Shape shape){
         PosConverter posConverter = new PosConverter(new Pair<Integer,Integer>(WIDTH, HEIGHT), shape);
         this.changeLocation(posConverter.getPos(dir));
         this.dir = dir;
     }
 
+    /**
+     * Raises the component by one step.
+     */
     public void raise(){
-        this.changeLocation(new Pos2D(this.getShape().getPos().x, this.getShape().getPos().y - SPEED));
+        this.changeLocation(new EntityPos2D(this.getShape().getPos().x, this.getShape().getPos().y - SPEED));
     }
 
+    /**
+     * Getter for the status.
+     */
     public Status getStatus(){
         return this.status;
     }
 
+    /**
+     * Setter for the Status.
+     * @param status the status that needs to be set.
+     */
     public void setStatus(Status status){
         this.status = status;
     }
 
+    /**
+     * Getter for the Direction.
+     * @return the actual Direction.
+     */
     public Directions getDirection(){
         return this.dir;
     }
     
+    /**
+     * Setter for the GunType.
+     * @param type the GunType that needs to be set.
+     */
     public void setType(GunTypes type){
         this.type = type;
     }
 
+    /**
+     * Getter for the GunType.
+     * @return the actual GunType.
+     */
     public GunTypes gType(){
         return this.type;
     }
