69256eafb9aa0f522a4dda065efbf1b66feabdd8
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-18 16:18:22 +0200

    Modified Constraint checker into a class that contains only static methods

--- app/src/main/java/ball/controller/IntersectionChecker.java
+++ app/src/main/java/ball/controller/IntersectionChecker.java
@@ -1,63 +1,12 @@
 package ball.controller;
 
-import ball.Boundary;
-import ball.ballAgent.BallAgent;
 import ball.physics.SpherePos2D;
 import ball.utils.Pair;
 import pangGuy.gui.Shape;
 
-public class ConstraintCheck {
-    private final double width;
-    private final double height;
-    
-    public ConstraintCheck(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    /**
-     * Check collisions beetween balls and walls.
-     * Expecially in X0 and Y1 there are offsets, due to strange behaviour
-     * of swing graphical interface (and MacOS).
-     * 
-     * @param t
-     *          the ball to check 
-     */
-    public void checkConstraints(BallAgent t) {
-        final var x =  (t.getBallPosition().x * this.width);
-        final var y =  (t.getBallPosition().y * this.height);
-        final int diameter = t.getBallPosition().getDiameter();
-        if (x < 0) {
-            t.applyConstraints(Boundary.X0.getValue() , Boundary.X0);
-        } else if (x + diameter >= this.width) {
-            t.applyConstraints(t.getBallPosition().x - (diameter * 0.0001), Boundary.X1);
-        } else if (y + 1.65 * diameter > this.height) { //very problematic in macos
-            t.applyConstraints(t.getBallPosition().y - 0.009, Boundary.Y1);
-        } else if (y < -1) {
-            t.applyConstraints(Boundary.Y0.getValue(), Boundary.Y0);
-        }
-    }
-
-    /**
-     * Checks wether a ball hits a {@link pangGuy.gui.Shape} or not.
-     * 
-     * @param entity
-     *          rectangular shaped entity
-     * @param ball 
-     *          spherical shaped entity
-     * @return
-     *          true if the two enemies collides
-     */
-    public boolean checkEnemyCollision(Shape entity, BallAgent ball) {
-        var bPos = new SpherePos2D(ball.getBallPosition().x * this.width,
-                        ball.getBallPosition().y *  this.height,
-                        ball.getBallPosition().getDimension(),
-                        ball.getSize());
-
-        return isCollision(bPos, entity);
-    }
-
-    private boolean isCollision(SpherePos2D ball, Shape rect) {
+public class IntersectionChecker {
+    //TOBE RENAMED
+    public static boolean isCollision(SpherePos2D ball, Shape rect) {
         var rectWidht = rect.getDimensions().getX();
         var rectHeight = rect.getDimensions().getY();
         Pair<Integer> rectCenter = new Pair<Integer>(rect.getPos().x + (int)(0.5*rectWidht),
