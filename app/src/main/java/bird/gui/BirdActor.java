49388aad88717e5229fe32d3f9fc0d39e75d5120
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-18 10:39:43 +0200

    added javadoc to bird actor

--- app/src/main/java/bird/gui/BirdActor.java
+++ app/src/main/java/bird/gui/BirdActor.java
@@ -9,16 +9,22 @@ import bird.utilities.BirdPos2D;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+/**
+ * This class is the bird itself. It defines his dimension thanks to the shape and it can
+ * change the bird's position.
+ */
 public class BirdActor extends JLabel{
-    
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1967410748214691856L;
     
 	private BirdShape s;
     private final BirdPNGLoader iLoader = new BirdPNGLoader();
 
+    /**
+     * Constructor that define the bird's dimension.
+     * @param s
+     *          bird's shape.
+     */
     public BirdActor(BirdShape s){
         this.s = s;
         
@@ -27,6 +33,11 @@ public class BirdActor extends JLabel{
         
     }
     
+    /**
+     * This method changes the position of the bird to ne new desired position.
+     * @param pos
+     *          new position.
+     */
     public void changeLocation(BirdPos2D pos){
 
         super.setLocation(pos.x, pos.y);
@@ -38,6 +49,9 @@ public class BirdActor extends JLabel{
         return this.s;
     }
 
+    /**
+     * Thanks to this method the bird is being paint correctly on the frame depending on his direction.
+     */
     public void paintComponent(Graphics g) {
 
         Graphics2D g2 = (Graphics2D) g;
