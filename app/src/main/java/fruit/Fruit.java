598006f4eb9cdb973437c942a6ac7ae27d00aa09
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-04-19 18:57:50 +0200

    update classes of package fruit

--- app/src/main/java/fruit/Fruit.java
+++ app/src/main/java/fruit/Fruit.java
@@ -1,5 +1,23 @@
 package fruit;
 
-public class Fruit {
+import javax.swing.JPanel;
+
+public class Fruit extends JPanel{
 	
+	private static final long serialVersionUID = 1L;
+	private boolean isActived;
+	private int score;
+	
+	public Fruit(int score) {
+		this.score = score;
+		this.isActived = false;
+	}
+	
+	public void activate() {
+		this.isActived = true;  
+	}
+	
+	public boolean isActive() {
+		return this.isActived;
+	}
 }
