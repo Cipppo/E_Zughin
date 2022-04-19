8dbc64190e105008aa865adab13221cc3bf859e1
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-19 13:35:27 +0200

    Deactivate controlPanel

--- app/src/main/java/mergeballs/MainMergeBalls.java
+++ app/src/main/java/mergeballs/MainMergeBalls.java
@@ -1,11 +1,11 @@
 package mergeballs;
 
-import mergeballs.gui.ControlPanelTest;
+
 import mergeballs.gui.GuiTest;
 
 public class MainMergeBalls {
     public static void main(String[] args){
         GuiTest gui = new GuiTest();
-        new ControlPanelTest(gui);
+        //new ControlPanelTest(gui);
     }
 }
