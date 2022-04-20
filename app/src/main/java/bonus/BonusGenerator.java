e6d538683606c7193fc91d9b0e2dba954b09b83a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-20 10:57:57 +0200

    Modify Spawn Y coord

--- app/src/main/java/bonus/BonusGenerator.java
+++ app/src/main/java/bonus/BonusGenerator.java
@@ -1,9 +1,6 @@
 package bonus;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
 import pangGuy.gui.Shape;
 import pangGuy.utilities.EntityPos2D;
 import pangGuy.utilities.Pair;
@@ -13,11 +10,10 @@ public class BonusGenerator {
     private static final int BONUS_WIDTH = 15;
     private static final int BONUS_HEIGHT = 15;
     
-    private List<Integer> baseScores;
+    private final int baseScore = 100;
     private final Pair<Integer, Integer> bounds;
 
     public BonusGenerator(Pair<Integer, Integer> bounds){
-        this.baseScores = new ArrayList<>(List.of(10, 100, 1000));
         this.bounds = bounds;
     }
 
@@ -27,12 +23,12 @@ public class BonusGenerator {
     }
 
     private int generateNextScore(){
-        return this.baseScores.get(this.getRandomInt(2)) * this.getRandomInt(10);
+        return this.baseScore * (this.getRandomInt(8) + 1); //8 + 1 in order to avoid 0: [1, 9]
     }
 
     private EntityPos2D generateRandomPos(){
         Random rand = new Random();
-        return new EntityPos2D(rand.nextInt(this.bounds.getX()), this.bounds.getY());
+        return new EntityPos2D(rand.nextInt(this.bounds.getX()), this.bounds.getY() - BONUS_HEIGHT);
     }
 
     private Shape generateRandomShape(){
