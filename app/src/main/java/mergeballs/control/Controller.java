9b2a0bb149de16ee65cdd149d93baecedd63c50a
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-21 13:57:07 +0200

    Create new Controller in order to increase encapsulation

---
+++ app/src/main/java/mergeballs/control/Controller.java
@@ -0,0 +1,59 @@
+package mergeballs.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ball.controller.BallBoundChecker;
+import ball.controller.Runner;
+import bonus.BonusHandler;
+import mergeballs.gui.VisualPanelTest;
+import mergeballs.gui.VisualTest;
+import pangGuy.actions.ActionApplier;
+import pangGuy.character.Hero;
+import player.Player;
+import powerUp.PowerUpHandler;
+
+public class Controller {
+    private final VisualTest visual;
+    private final VisualPanelTest panel;
+    private final EntityHandler entityHandler;
+    private final PickableHandler pickableHandler;
+    private final PauseHandler pauseHandler;
+    
+    public Controller(VisualTest visual) {
+        this.visual = visual;
+        this.panel = new VisualPanelTest(this.visual.getBounds().getX(), this.visual.getBounds().getY());
+        
+        Player player = new Player("Test");
+        Hero hero = new Hero();
+        new ActionApplier(this.visual, hero);
+
+        BallBoundChecker ballBoundChecker = new BallBoundChecker(this.visual.getBounds().getX(), this.visual.getBounds().getY());
+        Runner ballRunner = new Runner(5, ballBoundChecker);
+
+        this.entityHandler = new EntityHandler(this.visual, hero);
+        this.pickableHandler = new PickableHandler(this.visual.getHero(), 
+                        new PowerUpHandler(hero.getGset(), ballRunner, this.visual.getBounds()), 
+                        new BonusHandler(this.visual.getBounds()), 
+                        player, this.panel);
+
+        List<Pausable> pausables = new ArrayList<>();
+        for (final var elem : this.entityHandler.getPausable()) {
+            pausables.add(elem);
+        }
+        for (final var elem : this.pickableHandler.getPausable()) {
+            pausables.add(elem);
+        }
+
+        this.pauseHandler = new PauseHandler(this.visual, pausables);
+    }
+
+    public PauseHandler getPauseHandler() {
+        return this.pauseHandler;
+    }
+
+    public void startAll() {
+        this.entityHandler.start();
+        this.pickableHandler.start();
+    }
+}
