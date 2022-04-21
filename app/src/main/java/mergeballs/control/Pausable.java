cb99cf1958609b8aa856d3edadd0e0c1b6704554
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-21 11:46:32 +0200

    Implemented Pausable

---
+++ app/src/main/java/mergeballs/control/Pausable.java
@@ -0,0 +1,15 @@
+package mergeballs.control;
+
+public interface Pausable {
+
+    /**
+     * Pauses the Entity 
+     */
+    public void Pause();
+
+    /**
+     * Resume the Entity
+     */
+    public void Resume();
+    
+}
