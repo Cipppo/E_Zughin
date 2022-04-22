344c032a57527bd381fca351af3f16a6a505b639
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-22 10:36:37 +0200

    added lifeLabel in addAllLabel

--- app/src/main/java/stage/utils/AddAllLabels.java
+++ app/src/main/java/stage/utils/AddAllLabels.java
@@ -17,7 +17,7 @@ public class AddAllLabels {
         panel[1][1].add(new P1ScoreLabel());
         panel[0][3].add(new StageNameLabel());
         panel[2][3].add(new StageNumberLabel());
-        panel[3][3].add(new HighScoreLabel());
+        panel[3][3].add(new LifeLabel());
         panel[0][5].add(new PlayerTwoLabel());
         panel[1][6].add(new P2ScoreLabel());
     }
