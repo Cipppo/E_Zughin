b0d430e1b7bf43c4b997f3924a2017943e43b324
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-22 10:11:27 +0200

    Add JFrame to constructor

--- app/src/main/java/mergeballs/control/DeathHandler.java
+++ app/src/main/java/mergeballs/control/DeathHandler.java
@@ -1,15 +1,19 @@
 package mergeballs.control;
 
+import javax.swing.JFrame;
+
 import pangGuy.character.Hero;
 
 public class DeathHandler extends Thread{
     
     private final Hero hero;
     private boolean stop;
+    private final JFrame frame;
 
-    public DeathHandler(Hero hero){
+    public DeathHandler(Hero hero, JFrame frame){
         this.hero = hero;
         this.stop = false;
+        this.frame = frame;
     }
 
     @Override
@@ -17,7 +21,7 @@ public class DeathHandler extends Thread{
         while (!this.stop){
             try {
                 if(this.hero.getLifes() < 0){
-                    System.exit(0);
+                    this.frame.dispose();
                 }
                 Thread.sleep(40);
             } catch (InterruptedException e) {
