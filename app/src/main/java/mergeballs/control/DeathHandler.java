6b3e7e12f49ed478eb755f38da632008f5e2fcae
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-22 17:14:09 +0200

    first draft of end game choices

--- app/src/main/java/mergeballs/control/DeathHandler.java
+++ app/src/main/java/mergeballs/control/DeathHandler.java
@@ -1,6 +1,8 @@
 package mergeballs.control;
 
-import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import mergeballs.stage.StageGuiV2;
 
 import pangGuy.character.Hero;
 
@@ -8,9 +10,9 @@ public class DeathHandler extends Thread{
     
     private final Hero hero;
     private boolean stop;
-    private final JFrame frame;
+    private final StageGuiV2 frame; //temp, maybe wrap into a type
 
-    public DeathHandler(Hero hero, JFrame frame){
+    public DeathHandler(Hero hero, StageGuiV2 frame){
         this.hero = hero;
         this.stop = false;
         this.frame = frame;
@@ -18,16 +20,27 @@ public class DeathHandler extends Thread{
 
     @Override
     public void run(){
-        while (!this.stop){
+        while (!this.stop) {
             try {
-                if(this.hero.getLifes() < 0){
-                    this.frame.dispose();
+                if (this.hero.getLifes() < 3) {
+                    this.frame.terminator();
+                    if (this.askReplay() == 1) {
+                        this.stop = true;
+                        this.frame.dispose();
+                        System.exit(0);
+                    }
                 }
                 Thread.sleep(40);
             } catch (InterruptedException e) {
                 System.out.println("Thread.sleep() Exception: " + e.getMessage());
             }
         }
-    }   
+    }
 
+    private int askReplay() {
+        String options[] = {"yes", "no"};
+        return JOptionPane.showOptionDialog(null, "Do you want to play again?",
+                "GAME OVER",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+    }
 }
