b29d623bc9b8df39f6c7334f7ba478fce24b6387
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-04-23 22:58:40 +0200

    update players class

--- app/src/main/java/hallOfFameMenu/components/PlayersPanel.java
+++ app/src/main/java/hallOfFameMenu/components/PlayersPanel.java
@@ -38,6 +38,10 @@ public class PlayersPanel extends JPanel{
 		
 		Players p1 = new Players();
 		p1.Read();
+		Player player = new Player("MICHI");
+		player.increaseScore(300);
+		p1.add(player);
+		p1.Save();
 		
 		for(int i = 0; i < p1.get(); i++) {
 			String elem = Integer.toString(i+1);
