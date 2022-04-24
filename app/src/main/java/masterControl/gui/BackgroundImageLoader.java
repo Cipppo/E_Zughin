4d98c920f44cf67d6288f5ccdc5de05f04874ff3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-24 21:18:24 +0200

    Add javadoc

--- app/src/main/java/masterControl/gui/BackgroundImageLoader.java
+++ app/src/main/java/masterControl/gui/BackgroundImageLoader.java
@@ -4,10 +4,17 @@ import java.awt.image.BufferedImage;
 
 import stage.utils.MainImagesLoader;
 
+/**
+ * Image loader for the Background.
+ */
 public class BackgroundImageLoader {
 
     private MainImagesLoader loader = new MainImagesLoader();
     
+    /**
+     * Return the Background image.
+     * @return
+     */
     public BufferedImage getBackImage() {
         return loader.load("sprite/Backgrounds/mountFuji.png");
     }
