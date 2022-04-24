c1de95074a90839ac2fd3f4ee483ece2f11fe5cf
Stefano Furi <stefano.furi@studio.unibo.it>
2022-04-24 16:21:07 +0200

    removed exception in class definition and added simple description

--- app/src/main/java/mainMenu/MainVisual.java
+++ app/src/main/java/mainMenu/MainVisual.java
@@ -1,11 +1,13 @@
 package mainMenu;
 
-import java.io.FileNotFoundException;
-
 import menu.gui.Gui;
-
+/**
+ * Main class where the application start.
+ * This is the full main class, calling the starting menu,
+ * and then all the controllers and the stage of the game. 
+ */
 public class MainVisual {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		new Gui();
 	}
 }
